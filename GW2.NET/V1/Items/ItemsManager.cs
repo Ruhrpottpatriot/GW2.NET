96ae1e41e87c210a7548ce067a78097aa9388e11
bilong_cp <f8b59ba512f9592071703aea74feb8bf6b3cee9fB7Ha8N6K>
2013-06-01 18:50:44 +0000

    Added colors API implementation. Still needs editing to look nice.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET;C28237

---
+++ GW2.NET/V1/Items/ItemsManager.cs
@@ -0,0 +1,14 @@
+﻿using GW2DotNET.V1.Items.DataProvider;
+using GW2DotNET.V1.Items.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GW2DotNET.V1.Items
+{
+    public class ItemsManager
+    {
+        public ColorData Colors = new ColorData();
+    }
+}
