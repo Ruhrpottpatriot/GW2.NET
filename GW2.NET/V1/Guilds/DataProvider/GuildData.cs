34f48a44cb0983378bec6faa425220d7e78c6f84
bilong_cp <f8b59ba512f9592071703aea74feb8bf6b3cee9fB7Ha8N6K>
2013-06-23 16:03:50 +0000

    Implemented event-based async pattern in GuildData.cs.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET-Async;C29026

--- GW2.NET/V1/Guilds/DataProvider/GuildData.cs
+++ GW2.NET/V1/Guilds/DataProvider/GuildData.cs
@@ -13,13 +13,23 @@ using System.Linq;
 
 using GW2DotNET.V1.Guilds.Models;
 using GW2DotNET.V1.Infrastructure;
+using System.ComponentModel;
+using System.Threading;
+using System.Collections.Specialized;
 
 namespace GW2DotNET.V1.Guilds.DataProvider
 {
     /// <summary>
+    /// Delegate for GetGuildAsync completion event.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    public delegate void GetGuildCompletedEventHandler(object sender, GetGuildCompletedEventArgs e);
+
+    /// <summary>
     /// The guild data provider.
     /// </summary>
-    public class GuildData
+    public class GuildData : System.ComponentModel.Component
     {
         /// <summary>
         /// The guild cache.
@@ -33,6 +43,24 @@ namespace GW2DotNET.V1.Guilds.DataProvider
         private Gw2ApiManager gw2ApiManager;
         // ReSharper restore NotAccessedField.Local
 
+        /// <summary>
+        /// This event fires when a GetGuildAsync request completes.
+        /// </summary>
+        public event GetGuildCompletedEventHandler GetGuildCompleted;
+
+        /// <summary>
+        /// This event fires when the progress of a GetGuildAsync event is updated.
+        /// </summary>
+        public event ProgressChangedEventHandler ProgressChanged;
+
+        private SendOrPostCallback onProgressReportDelegate;
+
+        private SendOrPostCallback onCompletedDelegate;
+
+        private delegate void GetGuildWorkerEventHandler(Guid guildId, AsyncOperation asyncOp);
+
+        private HybridDictionary userStateToLifetime = new HybridDictionary();
+
         /// <summary>Initializes a new instance of the <see cref="GuildData"/> class.</summary>
         /// <param name="gw2ApiManager">The api Manager.</param>
         internal GuildData(Gw2ApiManager gw2ApiManager)
@@ -40,6 +68,8 @@ namespace GW2DotNET.V1.Guilds.DataProvider
             this.gw2ApiManager = gw2ApiManager;
 
             this.guildCache = new List<Guild>();
+
+            this.InitializeDelegates();
         }
 
         /// <summary>
@@ -97,6 +127,171 @@ namespace GW2DotNET.V1.Guilds.DataProvider
                 this.guildCache.Add(guildToReturn);
 
                 return guildToReturn;
+            }
+        }
+
+        /// <summary>
+        /// Initialize the delegates. This is called by the constructor.
+        /// </summary>
+        protected virtual void InitializeDelegates()
+        {
+            onProgressReportDelegate = new SendOrPostCallback(ReportProgress);
+
+            onCompletedDelegate = new SendOrPostCallback(GetGuildCompletedCallback);
+        }
+
+        // This method is invoked via the AsyncOperation object,
+        // so it is guaranteed to be executed on the correct thread.
+        private void GetGuildCompletedCallback(object operationState)
+        {
+            GetGuildCompletedEventArgs e = operationState as GetGuildCompletedEventArgs;
+
+            OnGetGuildCompleted(e);
+        }
+
+        private void OnGetGuildCompleted(GetGuildCompletedEventArgs e)
+        {
+            if (GetGuildCompleted != null)
+            {
+                GetGuildCompleted(this, e);
+            }
+        }
+
+        // This method is invoked via the AsyncOperation object,
+        // so it is guaranteed to be executed on the correct thread.
+        private void ReportProgress(object state)
+        {
+            ProgressChangedEventArgs e = state as ProgressChangedEventArgs;
+
+            OnProgressChanged(e);
+        }
+
+        private void OnProgressChanged(ProgressChangedEventArgs e)
+        {
+            if (ProgressChanged != null)
+            {
+                ProgressChanged(this, e);
+            }
+        }
+
+        private void GetGuildCompletionMethod(Guid guildID, Exception exception, bool canceled, AsyncOperation asyncOp)
+        {
+            if (!canceled)
+            {
+                lock (userStateToLifetime.SyncRoot)
+                {
+                    userStateToLifetime.Remove(asyncOp.UserSuppliedState);
+                }
+            }
+
+            GetGuildCompletedEventArgs e = new GetGuildCompletedEventArgs(guildID, exception, canceled, asyncOp.UserSuppliedState);
+
+            asyncOp.PostOperationCompleted(onCompletedDelegate, e);
+        }
+
+        // Utility method for determining if a task has been canceled.
+        private bool TaskCanceled(object taskId)
+        {
+            return (userStateToLifetime[taskId] == null);
+        }
+
+        /// <summary>
+        /// Performs the actual GetGuildAsync request. This method
+        /// is exectued on the worker thread.
+        /// </summary>
+        /// <param name="guildID"></param>
+        /// <param name="asyncOp"></param>
+        private void GetGuildWorker(Guid guildID, AsyncOperation asyncOp)
+        {
+            Exception e = null;
+            ProgressChangedEventArgs pe = null;
+
+            if (!TaskCanceled(asyncOp.UserSuppliedState))
+            {
+                try
+                {
+                    var guild = this[guildID];
+                    pe = new ProgressChangedEventArgs(100, asyncOp.UserSuppliedState);
+                    asyncOp.Post(this.onProgressReportDelegate, pe);
+                }
+                catch (Exception ex)
+                {
+                    e = ex;
+                }
+            }
+
+            this.GetGuildCompletionMethod(guildID, e, TaskCanceled(asyncOp.UserSuppliedState), asyncOp);
+        }
+
+        /// <summary>
+        /// Starts an asynchronous GetGuild request. TaskId must be unique.
+        /// </summary>
+        /// <param name="guildId"></param>
+        /// <param name="taskId"></param>
+        public virtual void GetGuildAsync(Guid guildId, object taskId)
+        {
+            AsyncOperation asyncOp = AsyncOperationManager.CreateOperation(taskId);
+
+            lock (userStateToLifetime.SyncRoot)
+            {
+                if (userStateToLifetime.Contains(taskId))
+                {
+                    throw new ArgumentException("Task ID parameter must be unique", "taskId");
+                }
+
+                userStateToLifetime[taskId] = asyncOp;
+            }
+
+            GetGuildWorkerEventHandler workerDelegate = new GetGuildWorkerEventHandler(this.GetGuildWorker);
+
+            workerDelegate.BeginInvoke(guildId, asyncOp, null, null);
+        }
+    }
+
+    /// <summary>
+    /// Holds the results of a GetGuildAysnc request.
+    /// </summary>
+    public class GetGuildCompletedEventArgs : AsyncCompletedEventArgs
+    {
+        private Guid guildID;
+        private Guild guild;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="guildID"></param>
+        /// <param name="e"></param>
+        /// <param name="canceled"></param>
+        /// <param name="state"></param>
+        public GetGuildCompletedEventArgs(Guid guildID, Exception e, bool canceled, object state)
+            : base(e, canceled, state)
+        {
+            this.guildID = guildID;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Guid GuildID
+        {
+            get
+            {
+                RaiseExceptionIfNecessary();
+
+                return this.guildID;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Guild Guild
+        {
+            get
+            {
+                RaiseExceptionIfNecessary();
+
+                return this.guild;
             }
         }
     }
