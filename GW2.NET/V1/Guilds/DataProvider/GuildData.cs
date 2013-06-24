d17f685fc3c83d7071212b9cb531d17478c1c140
bilong_cp <f8b59ba512f9592071703aea74feb8bf6b3cee9fB7Ha8N6K>
2013-06-24 16:16:49 +0000

    Implemented GetGuildByNameAsync. Moved the GuildData async implementations into their own files to keep them isolated and make everything cleaner.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET-Async;C29059

--- GW2.NET/V1/Guilds/DataProvider/GuildData.cs
+++ GW2.NET/V1/Guilds/DataProvider/GuildData.cs
@@ -9,84 +9,65 @@
 
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
-
 using GW2DotNET.V1.Guilds.Models;
 using GW2DotNET.V1.Infrastructure;
-using System.ComponentModel;
-using System.Threading;
-using System.Collections.Specialized;
 
 namespace GW2DotNET.V1.Guilds.DataProvider
 {
     /// <summary>
-    /// Delegate for GetGuildAsync completion event.
+    ///     The guild data provider.
     /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
-    public delegate void GetGuildCompletedEventHandler(object sender, GetGuildCompletedEventArgs e);
-
-    /// <summary>
-    /// The guild data provider.
-    /// </summary>
-    public class GuildData : System.ComponentModel.Component
+    public partial class GuildData : Component
     {
         /// <summary>
-        /// The guild cache.
+        ///     The guild cache.
         /// </summary>
         private readonly List<Guild> guildCache;
 
         /// <summary>
-        /// This object will be used to synchronize access to the guild cache.
-        /// You MUST lock this object any time you access the guild cache in
-        /// order to maintain thread safety.
+        ///     This object will be used to synchronize access to the guild cache.
+        ///     You MUST lock this object any time you access the guild cache in
+        ///     order to maintain thread safety.
         /// </summary>
-        private object guildCacheSyncObject = new object();
+        private readonly object guildCacheSyncObject = new object();
 
         /// <summary>
-        /// Stores the GW2ApiManager that instantiated this object
+        ///     Tracks the state of any async tasks.
+        /// </summary>
+        private readonly HybridDictionary userStateToLifetime = new HybridDictionary();
+
+        /// <summary>
+        ///     Stores the GW2ApiManager that instantiated this object
         /// </summary>
         // ReSharper disable NotAccessedField.Local
         private Gw2ApiManager gw2ApiManager;
+
         // ReSharper restore NotAccessedField.Local
 
         /// <summary>
-        /// This event fires when a GetGuildAsync request completes.
+        ///     Initializes a new instance of the <see cref="GuildData" /> class.
         /// </summary>
-        public event GetGuildCompletedEventHandler GetGuildCompleted;
-
-        /// <summary>
-        /// This event fires when the progress of a GetGuildAsync event is updated.
-        /// </summary>
-        public event ProgressChangedEventHandler ProgressChanged;
-
-        private SendOrPostCallback onProgressReportDelegate;
-
-        private SendOrPostCallback onCompletedDelegate;
-
-        private delegate void GetGuildWorkerEventHandler(Guid guildId, AsyncOperation asyncOp);
-
-        private HybridDictionary userStateToLifetime = new HybridDictionary();
-
-        /// <summary>Initializes a new instance of the <see cref="GuildData"/> class.</summary>
         /// <param name="gw2ApiManager">The api Manager.</param>
         internal GuildData(Gw2ApiManager gw2ApiManager)
         {
             this.gw2ApiManager = gw2ApiManager;
 
-            this.guildCache = new List<Guild>();
+            guildCache = new List<Guild>();
 
-            this.InitializeDelegates();
+            InitializeDelegates();
         }
 
         /// <summary>
-        /// Gets a single guild by ID from the cache if present, or from the API if not.
+        ///     Gets a single guild by ID from the cache if present, or from the API if not.
         /// </summary>
         /// <param name="guildId">
-        /// The guild id.
+        ///     The guild id.
         /// </param>
         /// <returns>
-        /// The <see cref="Guild"/>.
+        ///     The <see cref="Guild" />.
         /// </returns>
         public Guild this[Guid guildId]
         {
@@ -94,9 +75,9 @@ namespace GW2DotNET.V1.Guilds.DataProvider
             {
                 Guild guildToReturn;
 
-                lock (this.guildCacheSyncObject)
+                lock (guildCacheSyncObject)
                 {
-                    guildToReturn = this.guildCache.SingleOrDefault(g => g.Id == guildId);
+                    guildToReturn = guildCache.SingleOrDefault(g => g.Id == guildId);
                 }
 
                 if (guildToReturn.Id == Guid.Empty)
@@ -108,12 +89,12 @@ namespace GW2DotNET.V1.Guilds.DataProvider
 
                     guildToReturn = ApiCall.GetContent<Guild>("guild_details.json", arguments, ApiCall.Categories.Guild);
 
-                    lock (this.guildCacheSyncObject)
+                    lock (guildCacheSyncObject)
                     {
                         // A different thread could have added this guild to the cache already, so check first
-                        if (this.guildCache.SingleOrDefault(g => g.Id == guildId).Id == Guid.Empty)
+                        if (guildCache.SingleOrDefault(g => g.Id == guildId).Id == Guid.Empty)
                         {
-                            this.guildCache.Add(guildToReturn);
+                            guildCache.Add(guildToReturn);
                         }
                     }
                 }
@@ -123,19 +104,21 @@ namespace GW2DotNET.V1.Guilds.DataProvider
         }
 
         /// <summary>
-        /// Gets a single guild by name from the cache if present, or from the API if not.
+        ///     Gets a single guild by name from the cache if present, or from the API if not.
         /// </summary>
         /// <param name="guildName">The name of the guild</param>
-        /// <returns>The <see cref="Guild"/></returns>
+        /// <returns>
+        ///     The <see cref="Guild" />
+        /// </returns>
         public Guild this[string guildName]
         {
             get
             {
                 Guild guildToReturn;
 
-                lock (this.guildCacheSyncObject)
+                lock (guildCacheSyncObject)
                 {
-                    guildToReturn = this.guildCache.SingleOrDefault(g => g.Name == guildName);
+                    guildToReturn = guildCache.SingleOrDefault(g => g.Name == guildName);
                 }
 
                 if (guildToReturn.Id == Guid.Empty)
@@ -147,12 +130,12 @@ namespace GW2DotNET.V1.Guilds.DataProvider
 
                     guildToReturn = ApiCall.GetContent<Guild>("guild_details.json", arguments, ApiCall.Categories.Guild);
 
-                    lock (this.guildCacheSyncObject)
+                    lock (guildCacheSyncObject)
                     {
                         // A different thread could have added this guild to the cache already, so check first
-                        if (this.guildCache.SingleOrDefault(g => g.Name == guildName).Id == Guid.Empty)
+                        if (guildCache.SingleOrDefault(g => g.Name == guildName).Id == Guid.Empty)
                         {
-                            this.guildCache.Add(guildToReturn);
+                            guildCache.Add(guildToReturn);
                         }
                     }
                 }
@@ -162,171 +145,19 @@ namespace GW2DotNET.V1.Guilds.DataProvider
         }
 
         /// <summary>
-        /// Initialize the delegates. This is called by the constructor.
+        ///     Initialize the delegates. This is called by the constructor.
         /// </summary>
         protected virtual void InitializeDelegates()
         {
-            onProgressReportDelegate = new SendOrPostCallback(ReportProgress);
+            onGetGuildFromIdProgressReportDelegate = GetGuildFromIdReportProgress;
 
-            onCompletedDelegate = new SendOrPostCallback(GetGuildCompletedCallback);
-        }
-
-        // This method is invoked via the AsyncOperation object,
-        // so it is guaranteed to be executed on the correct thread.
-        private void GetGuildCompletedCallback(object operationState)
-        {
-            GetGuildCompletedEventArgs e = operationState as GetGuildCompletedEventArgs;
-
-            OnGetGuildCompleted(e);
-        }
-
-        private void OnGetGuildCompleted(GetGuildCompletedEventArgs e)
-        {
-            if (GetGuildCompleted != null)
-            {
-                GetGuildCompleted(this, e);
-            }
-        }
-
-        // This method is invoked via the AsyncOperation object,
-        // so it is guaranteed to be executed on the correct thread.
-        private void ReportProgress(object state)
-        {
-            ProgressChangedEventArgs e = state as ProgressChangedEventArgs;
-
-            OnProgressChanged(e);
-        }
-
-        private void OnProgressChanged(ProgressChangedEventArgs e)
-        {
-            if (ProgressChanged != null)
-            {
-                ProgressChanged(this, e);
-            }
-        }
-
-        private void GetGuildCompletionMethod(Guid guildID, Guild guild, Exception exception, bool canceled, AsyncOperation asyncOp)
-        {
-            if (!canceled)
-            {
-                lock (userStateToLifetime.SyncRoot)
-                {
-                    userStateToLifetime.Remove(asyncOp.UserSuppliedState);
-                }
-            }
-
-            GetGuildCompletedEventArgs e = new GetGuildCompletedEventArgs(guildID, guild, exception, canceled, asyncOp.UserSuppliedState);
-
-            asyncOp.PostOperationCompleted(onCompletedDelegate, e);
+            onGetGuildFromIdCompletedDelegate = GetGuildFromIdCompletedCallback;
         }
 
         // Utility method for determining if a task has been canceled.
         private bool TaskCanceled(object taskId)
         {
             return (userStateToLifetime[taskId] == null);
-        }
-
-        /// <summary>
-        /// Performs the actual GetGuildAsync request. This method
-        /// is exectued on the worker thread.
-        /// </summary>
-        /// <param name="guildID"></param>
-        /// <param name="asyncOp"></param>
-        private void GetGuildWorker(Guid guildID, AsyncOperation asyncOp)
-        {
-            Exception e = null;
-            ProgressChangedEventArgs pe = null;
-            Guild guildToReturn = new Guild();
-
-            if (!TaskCanceled(asyncOp.UserSuppliedState))
-            {
-                try
-                {
-                    guildToReturn = this[guildID];
-                    pe = new ProgressChangedEventArgs(100, asyncOp.UserSuppliedState);
-                    asyncOp.Post(this.onProgressReportDelegate, pe);
-                }
-                catch (Exception ex)
-                {
-                    e = ex;
-                }
-            }
-
-            this.GetGuildCompletionMethod(guildID, guildToReturn, e, TaskCanceled(asyncOp.UserSuppliedState), asyncOp);
-        }
-
-        /// <summary>
-        /// Starts an asynchronous GetGuild request. TaskId must be unique.
-        /// </summary>
-        /// <param name="guildId"></param>
-        /// <param name="taskId"></param>
-        public virtual void GetGuildAsync(Guid guildId, object taskId)
-        {
-            AsyncOperation asyncOp = AsyncOperationManager.CreateOperation(taskId);
-
-            lock (userStateToLifetime.SyncRoot)
-            {
-                if (userStateToLifetime.Contains(taskId))
-                {
-                    throw new ArgumentException("Task ID parameter must be unique", "taskId");
-                }
-
-                userStateToLifetime[taskId] = asyncOp;
-            }
-
-            GetGuildWorkerEventHandler workerDelegate = new GetGuildWorkerEventHandler(this.GetGuildWorker);
-
-            workerDelegate.BeginInvoke(guildId, asyncOp, null, null);
-        }
-    }
-
-    /// <summary>
-    /// Holds the results of a GetGuildAysnc request.
-    /// </summary>
-    public class GetGuildCompletedEventArgs : AsyncCompletedEventArgs
-    {
-        private Guid guildID;
-        private Guild guild;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="guildID"></param>
-        /// <param name="guild"></param>
-        /// <param name="e"></param>
-        /// <param name="canceled"></param>
-        /// <param name="state"></param>
-        public GetGuildCompletedEventArgs(Guid guildID, Guild guild, Exception e, bool canceled, object state)
-            : base(e, canceled, state)
-        {
-            this.guildID = guildID;
-            this.guild = guild;
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public Guid GuildID
-        {
-            get
-            {
-                RaiseExceptionIfNecessary();
-
-                return this.guildID;
-            }
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public Guild Guild
-        {
-            get
-            {
-                RaiseExceptionIfNecessary();
-
-                return this.guild;
-            }
         }
     }
 }
