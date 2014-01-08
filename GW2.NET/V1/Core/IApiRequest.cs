08a7ac0241e5b3d8b1fbe28b1bd8d31d2dcc7722
Steven Liekens <steven.liekens@gmail.com>
2014-01-08 18:55:23 +0000

    Fixing StyleCop violations for IApiRequest.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET-Http;C32923

--- GW2.NET/V1/Core/IApiRequest.cs
+++ GW2.NET/V1/Core/IApiRequest.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IApiRequest.cs" company="GW2.Net Coding Team">
+//   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+using System;
 using System.Threading.Tasks;
+
 namespace GW2DotNET.V1.Core
 {
+    /// <summary>
+    /// Provides the base interface for HTTP requests targeting the Guild Wars 2 API.
+    /// </summary>
     public interface IApiRequest
     {
+        /// <summary>
+        /// Sends this request to the specified API client and retrieves a response whose content can be mapped to the specified type.
+        /// </summary>
+        /// <typeparam name="TResponse">The type of the response content.</typeparam>
+        /// <param name="handler">The HTTP client that sends the request over a network and returns a response.</param>
+        /// <returns>Returns the response content as an object instance of the specified type.</returns>
         IApiResponse<TResponse> GetResponse<TResponse>(IApiClient handler);
+
+        /// <summary>
+        /// Asynchronously sends this request to the specified API client and retrieves a response whose content can be mapped to the specified type.
+        /// </summary>
+        /// <typeparam name="TResponse">The type of the response content.</typeparam>
+        /// <param name="handler">The HTTP client that sends the request over a network and returns a response.</param>
+        /// <returns>Returns the response content as an object instance of the specified type.</returns>
         Task<IApiResponse<TResponse>> GetResponseAsync<TResponse>(IApiClient handler);
     }
 }
