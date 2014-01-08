fd9eb5658965a5c7dc17ffd4bf7a10bec4e0941d
Steven Liekens <steven.liekens@gmail.com>
2014-01-08 18:07:02 +0000

    Fixing StyleCop violations for IApiClient.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET-Http;C32922

--- GW2.NET/V1/Core/IApiClient.cs
+++ GW2.NET/V1/Core/IApiClient.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IApiClient.cs" company="GW2.Net Coding Team">
+//   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+using System;
 using System.Threading.Tasks;
+
 namespace GW2DotNET.V1.Core
 {
+    /// <summary>
+    /// Provides the base interface for HTTP clients targeting the Guild Wars 2 API.
+    /// </summary>
     public interface IApiClient
     {
+        /// <summary>
+        /// Send a request and return a response whose content can be mapped to the specified type.
+        /// </summary>
+        /// <typeparam name="TResponse">The type of the response content.</typeparam>
+        /// <param name="request">The service request that targets a specific API endpoint.</param>
+        /// <returns>Returns the response content as an object instance of the specified type.</returns>
         IApiResponse<TResponse> Send<TResponse>(IApiRequest request);
+
+        /// <summary>
+        /// Asynchronously send a request and return a response whose content can be mapped to the specified type.
+        /// </summary>
+        /// <typeparam name="TResponse">The type of the response content.</typeparam>
+        /// <param name="request">The service request that targets a specific API endpoint.</param>
+        /// <returns>Returns the response content as an object instance of the specified type.</returns>
         Task<IApiResponse<TResponse>> SendAsync<TResponse>(IApiRequest request);
     }
 }
