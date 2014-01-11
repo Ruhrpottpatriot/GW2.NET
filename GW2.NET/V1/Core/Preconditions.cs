ca76dd503c5612f029c5d6c7b1e61e8bd2d4841e
Steven Liekens <steven.liekens@gmail.com>
2014-01-11 13:05:43 +0000

    Adding a little utility class for input validation: Preconditions.cs
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET-Http;C33030

---
+++ GW2.NET/V1/Core/Preconditions.cs
@@ -0,0 +1,62 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Preconditions.cs" company="GW2.Net Coding Team">
+//   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GW2DotNET.V1.Core
+{
+    /// <summary>
+    /// Utility class. Provides static methods for validating arguments.
+    /// </summary>
+    internal static class Preconditions
+    {
+        /// <summary>
+        /// Ensures that the specified value is not a null reference.
+        /// </summary>
+        /// <typeparam name="T">The value type.</typeparam>
+        /// <param name="value">The actual value.</param>
+        /// <exception cref="ArgumentNullException">The exception that is thrown when the specified value is null.</exception>
+        /// <returns>The specified value.</returns>
+        internal static T EnsureNotNull<T>(T value) where T : class
+        {
+            return Preconditions.EnsureNotNull(value, null, null);
+        }
+
+        /// <summary>
+        /// Ensures that the specified value is not a null reference.
+        /// </summary>
+        /// <typeparam name="T">The value type.</typeparam>
+        /// <param name="value">The actual value.</param>
+        /// <param name="paramName">The name of the parameter.</param>
+        /// <exception cref="ArgumentNullException">The exception that is thrown when the specified value is null.</exception>
+        /// <returns>The specified value.</returns>
+        internal static T EnsureNotNull<T>(T value, string paramName) where T : class
+        {
+            return Preconditions.EnsureNotNull(value, paramName, null);
+        }
+
+        /// <summary>
+        /// Ensures that the specified value is not a null reference.
+        /// </summary>
+        /// <typeparam name="T">The value type.</typeparam>
+        /// <param name="value">The actual value.</param>
+        /// <param name="paramName">The name of the parameter.</param>
+        /// <param name="message">A message that describes the error.</param>
+        /// <exception cref="ArgumentNullException">The exception that is thrown when the specified value is null.</exception>
+        /// <returns>The specified value.</returns>
+        internal static T EnsureNotNull<T>(T value, string paramName, string message) where T : class
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(paramName, message);
+            }
+
+            return value;
+        }
+    }
+}
