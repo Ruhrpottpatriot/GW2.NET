a7be8519a0f549c2ecdba892f6093a6434d3450b
Steven Liekens <steven.liekens@gmail.com>
2014-01-12 16:48:41 +0000

    Adding an utility class that lists the supported languages.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET-Http;C33122

---
+++ GW2.NET/V1/Core/SupportedLanguages.cs
@@ -0,0 +1,47 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SupportedLanguages.cs" company="GW2.Net Coding Team">
+//   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+using System.Globalization;
+
+namespace GW2DotNET.V1.Core
+{
+    /// <summary>
+    /// Utility class. Provides static properties enumerating the supported languages.
+    /// </summary>
+    public static class SupportedLanguages
+    {
+        /// <summary>
+        /// Gets the English language.
+        /// </summary>
+        public static CultureInfo English
+        {
+            get { return CultureInfo.GetCultureInfo("en"); }
+        }
+
+        /// <summary>
+        /// Gets the French language.
+        /// </summary>
+        public static CultureInfo French
+        {
+            get { return CultureInfo.GetCultureInfo("fr"); }
+        }
+
+        /// <summary>
+        /// Gets the German language.
+        /// </summary>
+        public static CultureInfo German
+        {
+            get { return CultureInfo.GetCultureInfo("de"); }
+        }
+
+        /// <summary>
+        /// Gets the Spanish language.
+        /// </summary>
+        public static CultureInfo Spanish
+        {
+            get { return CultureInfo.GetCultureInfo("es"); }
+        }
+    }
+}
