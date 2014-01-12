c77298ce1b3338d9a2370bc36c99988fe7400c15
Steven Liekens <steven.liekens@gmail.com>
2014-01-12 13:59:16 +0000

    - Adding support for the "build.json" API. - Adding a missing "using" statement.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET-Http;C33105

---
+++ GW2.NET/V1/Core/Build/BuildRequest.cs
@@ -0,0 +1,47 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BuildRequest.cs" company="GW2.Net Coding Team">
+//   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+using System;
+using System.Threading.Tasks;
+
+namespace GW2DotNET.V1.Core.Build
+{
+    /// <summary>
+    /// Represents a request for the current build ID of the game.
+    /// </summary>
+    /// <remarks>
+    /// See <a href="http://wiki.guildwars2.com/wiki/API:1/build"/> for more information.
+    /// </remarks>
+    public class BuildRequest : ApiRequest
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BuildRequest"/> class.
+        /// </summary>
+        public BuildRequest()
+            : base(new Uri(Resources.Build, UriKind.Relative))
+        {
+        }
+
+        /// <summary>
+        /// Sends this request to the specified <see cref="ApiClient"/> and retrieves a response whose content is of type <see cref="BuildResponse"/>.
+        /// </summary>
+        /// <param name="handler">The <see cref="ApiClient"/> that sends the request over a network and returns an instance of type <see cref="ApiResponse{TContent}"/>.</param>
+        /// <returns>Returns an instance of type <see cref="BuildResponse"/>.</returns>
+        public IApiResponse<BuildResponse> GetResponse(IApiClient handler)
+        {
+            return base.GetResponse<BuildResponse>(handler);
+        }
+
+        /// <summary>
+        /// Asynchronously sends this request to the specified <see cref="ApiClient"/> and retrieves a response whose content is of type <see cref="BuildResponse"/>.
+        /// </summary>
+        /// <param name="handler">The <see cref="ApiClient"/> that sends the request over a network and returns an instance of type <see cref="ApiResponse{TContent}"/>.</param>
+        /// <returns>Returns an instance of type <see cref="BuildResponse"/>.</returns>
+        public Task<IApiResponse<BuildResponse>> GetResponseAsync(IApiClient handler)
+        {
+            return base.GetResponseAsync<BuildResponse>(handler);
+        }
+    }
+}
--- GW2.NET/V1/Core/Build/BuildResponse.cs
+++ GW2.NET/V1/Core/Build/BuildResponse.cs
@@ -1,36 +1,36 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="ItemsResponse.cs" company="GW2.Net Coding Team">
+// <copyright file="BuildResponse.cs" company="GW2.Net Coding Team">
 //   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 using System;
-using System.Collections.Generic;
 using Newtonsoft.Json;
 
-namespace GW2DotNET.V1.Core.Items
+namespace GW2DotNET.V1.Core.Build
 {
     /// <summary>
-    /// Represents a response that is the result of an <see cref="ItemsRequest"/>.
+    /// Represents a response that is the result of a <see cref="BuildRequest"/>.
     /// </summary>
     /// <remarks>
-    /// See <a href="http://wiki.guildwars2.com/wiki/API:1/items"/> for more information.
+    /// See <a href="http://wiki.guildwars2.com/wiki/API:1/build"/> for more information.
     /// </remarks>
-    public class ItemsResponse
+    public class BuildResponse
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="ItemsResponse"/> class.
+        /// Initializes a new instance of the <see cref="BuildResponse"/> class.
         /// </summary>
-        /// <param name="items">The list of all discovered item IDs.</param>
-        public ItemsResponse(List<int> items)
+        /// <param name="buildId">The build ID.</param>
+        [JsonConstructor]
+        public BuildResponse(int buildId)
         {
-            this.Items = items;
+            this.BuildId = buildId;
         }
 
         /// <summary>
-        /// Gets a list of all discovered item IDs.
+        /// Gets the current build ID of the game.
         /// </summary>
-        [JsonProperty("items")]
-        public List<int> Items { get; private set; }
+        [JsonProperty("build_id")]
+        public int BuildId { get; private set; }
 
         /// <summary>
         /// Gets the JSON representation of this instance.
