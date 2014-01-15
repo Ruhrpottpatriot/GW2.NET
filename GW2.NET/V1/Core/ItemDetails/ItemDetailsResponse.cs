47e94f14fcf1bdb118a14ecfe70630c61e8a861f
Steven Liekens <steven.liekens@gmail.com>
2014-01-15 17:23:52 +0000

    Adding stubs for the "item_details.json" API.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET-Http;C33343

---
+++ GW2.NET/V1/Core/ItemDetails/ItemDetailsResponse.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ItemDetailsResponse.cs" company="GW2.Net Coding Team">
+//   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GW2DotNET.V1.Core.ItemDetails
+{
+    /// <summary>
+    /// Represents a response that is the result of an <see cref="ItemDetailsRequest"/>.
+    /// </summary>
+    /// <remarks>
+    /// See <a href="http://wiki.guildwars2.com/wiki/API:1/item_details"/> for more information.
+    /// </remarks>
+    public class ItemDetailsResponse
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ItemDetailsResponse"/> class.
+        /// </summary>
+        public ItemDetailsResponse()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
