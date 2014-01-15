d0bb9c051eab47c11c735af43ef541ab4c27f005
Steven Liekens <steven.liekens@gmail.com>
2014-01-15 18:13:37 +0000

    Adding stubs for the "continents.json" API.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET-Http;C33351

---
+++ GW2.NET/V1/Core/Continents/ContinentsRequest.cs
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ContinentsRequest.cs" company="GW2.Net Coding Team">
+//   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GW2DotNET.V1.Core.Continents
+{
+    /// <summary>
+    /// Represents a request for static information about the continents.
+    /// </summary>
+    /// <remarks>
+    /// See <a href="http://wiki.guildwars2.com/wiki/API:1/continents"/> for more information.
+    /// </remarks>
+    public class ContinentsRequest : ApiRequest
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ContinentsRequest"/> class.
+        /// </summary>
+        public ContinentsRequest()
+            : base(new Uri(Resources.Continents, UriKind.Relative))
+        {
+        }
+
+        /// <summary>
+        /// Sends this request to the specified <see cref="ApiClient"/> and retrieves a response whose content is of type <see cref="ContinentsResponse"/>.
+        /// </summary>
+        /// <param name="handler">The <see cref="ApiClient"/> that sends the request over a network and returns an instance of type <see cref="ApiResponse{TContent}"/>.</param>
+        /// <returns>Returns an instance of type <see cref="ContinentsResponse"/>.</returns>
+        public IApiResponse<ContinentsResponse> GetResponse(IApiClient handler)
+        {
+            return base.GetResponse<ContinentsResponse>(handler);
+        }
+
+        /// <summary>
+        /// Asynchronously sends this request to the specified <see cref="ApiClient"/> and retrieves a response whose content is of type <see cref="ContinentsResponse"/>.
+        /// </summary>
+        /// <param name="handler">The <see cref="ApiClient"/> that sends the request over a network and returns an instance of type <see cref="ApiResponse{TContent}"/>.</param>
+        /// <returns>Returns an instance of type <see cref="ContinentsResponse"/>.</returns>
+        public Task<IApiResponse<ContinentsResponse>> GetResponseAsync(IApiClient handler)
+        {
+            return base.GetResponseAsync<ContinentsResponse>(handler);
+        }
+    }
+}
---
+++ GW2.NET/V1/Core/Continents/ContinentsResponse.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ContinentsResponse.cs" company="GW2.Net Coding Team">
+//   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GW2DotNET.V1.Core.Continents
+{
+    /// <summary>
+    /// Represents a response that is the result of a <see cref="ContinentsRequest"/>.
+    /// </summary>
+    /// <remarks>
+    /// See <a href="http://wiki.guildwars2.com/wiki/API:1/continents"/> for more information.
+    /// </remarks>
+    public class ContinentsResponse
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ContinentsResponse"/> class.
+        /// </summary>
+        public ContinentsResponse()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
