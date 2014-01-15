599e45b846c0874288231ed4d2c2a9b774d25648
Steven Liekens <steven.liekens@gmail.com>
2014-01-15 18:49:44 +0000

    Adding stubs for the "wvw/matches.json" API.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET-Http;C33355

---
+++ GW2.NET/V1/Core/WvW/Matches/MatchesRequest.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MatchesRequest.cs" company="GW2.Net Coding Team">
+//   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+using System;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace GW2DotNET.V1.Core.WvW.Matches
+{
+    /// <summary>
+    /// Represents a request for a list of the currently running World versus World matches, with the participating worlds included in the result.
+    /// </summary>
+    /// <remarks>
+    /// See <a href="http://wiki.guildwars2.com/wiki/API:1/wvw/matches"/> for more information.
+    /// </remarks>
+    public class MatchesRequest : ApiRequest
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MatchesRequest"/> class.
+        /// </summary>
+        public MatchesRequest()
+            : base(new Uri(Resources.Matches, UriKind.Relative))
+        {
+        }
+
+        /// <summary>
+        /// Sends this request to the specified <see cref="ApiClient"/> and retrieves a response whose content is of type <see cref="MatchesResponse"/>.
+        /// </summary>
+        /// <param name="handler">The <see cref="ApiClient"/> that sends the request over a network and returns an instance of type <see cref="ApiResponse{TContent}"/>.</param>
+        /// <returns>Returns an instance of type <see cref="MatchesResponse"/>.</returns>
+        public IApiResponse<MatchesResponse> GetResponse(IApiClient handler)
+        {
+            return base.GetResponse<MatchesResponse>(handler);
+        }
+
+        /// <summary>
+        /// Asynchronously sends this request to the specified <see cref="ApiClient"/> and retrieves a response whose content is of type <see cref="MatchesResponse"/>.
+        /// </summary>
+        /// <param name="handler">The <see cref="ApiClient"/> that sends the request over a network and returns an instance of type <see cref="ApiResponse{TContent}"/>.</param>
+        /// <returns>Returns an instance of type <see cref="MatchesResponse"/>.</returns>
+        public Task<IApiResponse<MatchesResponse>> GetResponseAsync(IApiClient handler)
+        {
+            return base.GetResponseAsync<MatchesResponse>(handler);
+        }
+    }
+}
---
+++ GW2.NET/V1/Core/WvW/Matches/MatchesResponse.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MatchesResponse.cs" company="GW2.Net Coding Team">
+//   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+using System;
+
+namespace GW2DotNET.V1.Core.WvW.Matches
+{
+    /// <summary>
+    /// Represents a response that is the result of a <see cref="MatchesRequest"/>.
+    /// </summary>
+    /// <remarks>
+    /// See <a href="http://wiki.guildwars2.com/wiki/API:1/wvw/matches"/> for more information.
+    /// </remarks>
+    public class MatchesResponse
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MatchesResponse"/> class.
+        /// </summary>
+        public MatchesResponse()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
