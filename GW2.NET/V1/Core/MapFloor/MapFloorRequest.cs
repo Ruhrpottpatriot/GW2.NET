1f9278a00378f886c6acbf8464ee11b8dc7ba48b
Steven Liekens <steven.liekens@gmail.com>
2014-01-15 18:36:05 +0000

    Adding stubs for the "map_floor.json" API.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET-Http;C33353

---
+++ GW2.NET/V1/Core/MapFloor/MapFloorRequest.cs
@@ -0,0 +1,66 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MapFloorRequest.cs" company="GW2.Net Coding Team">
+//   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+using System;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace GW2DotNET.V1.Core.MapFloor
+{
+    /// <summary>
+    /// Represents a request for details regarding a map floor, used to populate a world map.
+    /// </summary>
+    /// <remarks>
+    /// See <a href="http://wiki.guildwars2.com/wiki/API:1/map_floor"/> for more information.
+    /// </remarks>
+    public class MapFloorRequest : ApiRequest
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MapFloorRequest"/> class.
+        /// </summary>
+        /// <param name="continentId">The continent ID.</param>
+        /// <param name="floor">The map floor.</param>
+        public MapFloorRequest(int continentId, int floor)
+            : base(new Uri(Resources.MapFloor + "?continent_id={continent_id}&floor={floor}", UriKind.Relative))
+        {
+            this.AddUrlSegment("continent_id", continentId.ToString());
+            this.AddUrlSegment("floor", floor.ToString());
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MapFloorRequest"/> class using the specified language.
+        /// </summary>
+        /// <param name="continentId">The continent ID.</param>
+        /// <param name="floor">The map floor.</param>
+        /// <param name="language">The output language. Supported values are enumerated in <see cref="SupportedLanguages"/>.</param>
+        public MapFloorRequest(int continentId, int floor, CultureInfo language)
+            : base(new Uri(Resources.MapFloor + "?continent_id={continent_id}&floor={floor}&lang={language}", UriKind.Relative))
+        {
+            this.AddUrlSegment("continent_id", continentId.ToString());
+            this.AddUrlSegment("floor", floor.ToString());
+            this.AddUrlSegment("language", language.TwoLetterISOLanguageName);
+        }
+
+        /// <summary>
+        /// Sends this request to the specified <see cref="ApiClient"/> and retrieves a response whose content is of type <see cref="MapFloorResponse"/>.
+        /// </summary>
+        /// <param name="handler">The <see cref="ApiClient"/> that sends the request over a network and returns an instance of type <see cref="ApiResponse{TContent}"/>.</param>
+        /// <returns>Returns an instance of type <see cref="MapFloorResponse"/>.</returns>
+        public IApiResponse<MapFloorResponse> GetResponse(IApiClient handler)
+        {
+            return base.GetResponse<MapFloorResponse>(handler);
+        }
+
+        /// <summary>
+        /// Asynchronously sends this request to the specified <see cref="ApiClient"/> and retrieves a response whose content is of type <see cref="MapFloorResponse"/>.
+        /// </summary>
+        /// <param name="handler">The <see cref="ApiClient"/> that sends the request over a network and returns an instance of type <see cref="ApiResponse{TContent}"/>.</param>
+        /// <returns>Returns an instance of type <see cref="MapFloorResponse"/>.</returns>
+        public Task<IApiResponse<MapFloorResponse>> GetResponseAsync(IApiClient handler)
+        {
+            return base.GetResponseAsync<MapFloorResponse>(handler);
+        }
+    }
+}
---
+++ GW2.NET/V1/Core/MapFloor/MapFloorResponse.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MapFloorResponse.cs" company="GW2.Net Coding Team">
+//   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+using System;
+
+namespace GW2DotNET.V1.Core.MapFloor
+{
+    /// <summary>
+    /// Represents a response that is the result of a <see cref="MapFloorRequest"/>.
+    /// </summary>
+    /// <remarks>
+    /// The returned data only contains static content. Dynamic content, such as vendors, is not currently available.
+    /// See <a href="http://wiki.guildwars2.com/wiki/API:1/map_floor"/> for more information.
+    /// </remarks>
+    public class MapFloorResponse
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MapFloorResponse"/> class.
+        /// </summary>
+        public MapFloorResponse()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
