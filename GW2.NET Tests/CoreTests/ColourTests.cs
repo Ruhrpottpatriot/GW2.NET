0a3af6cc6649aed2b3faab913488791aab4fd2b6
Ruhrpottpatriot <Ruhrpottpatriot@gmx.de>
2014-02-13 22:16:33 +0000

    - Added separate test for colours, testing each language and asynchronous and synchronous operations.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET-Http;C34155

---
+++ GW2.NET Tests/CoreTests/ColourTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using GW2DotNET.V1.Core;
+using GW2DotNET.V1.Core.Colors;
+using GW2DotNET.V1.Core.Colors.Models;
+
+using NUnit.Framework;
+
+namespace GW2DotNET_Tests.CoreTests
+{
+    [TestFixture]
+    class ColourTests
+    {
+        [Test]
+        public void GetColours()
+        {
+            ColorsRequest colorsRequest = new ColorsRequest(SupportedLanguages.English);
+
+            ColorsResponse colorsResponse = colorsRequest.GetResponse(ApiClient.Create()).DeserializeResponse();
+
+            IDictionary<int, Dye> colours = colorsResponse.Colors;
+
+            Assert.IsNotNull(colours);
+            Assert.IsNotEmpty(colours);
+        }
+
+        [Test]
+        public async void GetColoursAsync()
+        {
+            ColorsRequest request = new ColorsRequest(SupportedLanguages.English);
+
+            ColorsResponse colorsResponse = (await request.GetResponseAsync(ApiClient.Create())).DeserializeResponse();
+
+            IDictionary<int, Dye> colours = colorsResponse.Colors;
+
+            Assert.IsNotNull(colours);
+            Assert.IsNotEmpty(colours);
+        }
+
+        [Test]
+        public void GetColoursGerman()
+        {
+
+            ColorsRequest request = new ColorsRequest(SupportedLanguages.German);
+
+            ColorsResponse colorsResponse = request.GetResponse(ApiClient.Create()).DeserializeResponse();
+
+            IDictionary<int, Dye> colours = colorsResponse.Colors;
+
+            Assert.IsNotNull(colours);
+            Assert.IsNotEmpty(colours);
+        }
+
+        [Test]
+        public void GetColoursFrench()
+        {
+
+            ColorsRequest request = new ColorsRequest(SupportedLanguages.French);
+
+            ColorsResponse colorsResponse = request.GetResponse(ApiClient.Create()).DeserializeResponse();
+
+            IDictionary<int, Dye> colours = colorsResponse.Colors;
+
+            Assert.IsNotNull(colours);
+            Assert.IsNotEmpty(colours);
+        }
+
+        [Test]
+        public void GetColoursSpanish()
+        {
+
+            ColorsRequest request = new ColorsRequest(SupportedLanguages.Spanish);
+
+            ColorsResponse colorsResponse = request.GetResponse(ApiClient.Create()).DeserializeResponse();
+
+            IDictionary<int, Dye> colours = colorsResponse.Colors;
+
+            Assert.IsNotNull(colours);
+            Assert.IsNotEmpty(colours);
+        }
+    }
+}
--- GW2.NET Tests/CoreTests/Miscellaneous.cs
+++ GW2.NET Tests/CoreTests/Miscellaneous.cs
@@ -33,25 +33,6 @@ namespace GW2DotNET_Tests.CoreTests
         }
 
         [Test]
-        public void GetColours()
-        {
-            ColorsRequest colorsRequest = new ColorsRequest(new CultureInfo("en"));
-
-            ColorsResponse colorsResponse = colorsRequest.GetResponse(ApiClient.Create(new Version(1, 0))).DeserializeResponse();
-
-            IDictionary<int, Dye> colours = colorsResponse.Colors;
-
-            Assert.IsNotNull(colours);
-            Assert.IsNotEmpty(colours);
-
-            Trace.WriteLine(string.Format("Number of colours: {0}", colours.Count));
-
-            KeyValuePair<int, Dye> dye = colours.First(colour => colour.Key == 126);
-
-            Trace.WriteLine(string.Format("{0}", dye.Value.Name));
-        }
-
-        [Test]
         public void GetFiles()
         {
             FilesRequest filesRequest = new FilesRequest();
