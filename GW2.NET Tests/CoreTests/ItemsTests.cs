0ac5be67e41be2a51981f80ba333cea22300cd68
Steven Liekens <steven.liekens@gmail.com>
2014-02-22 20:31:56 +0000

    Improving support for 'items.json'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET-Http;C34533

---
+++ GW2.NET Tests/CoreTests/ItemsTests.cs
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ItemsTests.cs" company="GW2.Net Coding Team">
+//   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+// <summary>
+//   Defines the ItemsTests type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Diagnostics;
+
+using GW2DotNET.V1.Core;
+using GW2DotNET.V1.Core.ItemsInformation;
+using GW2DotNET.V1.Core.ItemsInformation.Catalog;
+using NUnit.Framework;
+
+namespace GW2DotNET_Tests.CoreTests
+{
+    [TestFixture]
+    public class ItemsTests
+    {
+        private IApiClient client;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.client = ApiClient.Create(new Version(1, 0));
+        }
+
+        [Test]
+        public void GetItemsCatalog()
+        {
+            var request  = new ItemsRequest();
+            var response = request.GetResponse(client);
+
+            Assert.IsTrue(response.IsSuccessStatusCode);
+            Assert.IsTrue(response.IsJsonResponse);
+
+            var itemsResult = response.Deserialize();
+
+            Assert.IsNotNull(itemsResult);
+            Assert.IsEmpty(itemsResult.ExtensionData, "The '{0}' class is missing one or more properties.", typeof(ItemsResult).FullName);
+
+            Assert.IsNotNull(itemsResult.Items);
+            Assert.IsEmpty(itemsResult.Items.ExtensionData, "The '{0}' class is missing one or more properties.", typeof(Items).FullName);
+
+            Trace.WriteLine(string.Format("Number of items: {0}", itemsResult.Items.Count));
+        }
+    }
+}
--- GW2.NET/V1/Core/ItemsInformation/Catalog/ItemsResult.cs
+++ GW2.NET/V1/Core/ItemsInformation/Catalog/ItemsResult.cs
@@ -1,26 +1,22 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="ItemsResponse.cs" company="GW2.Net Coding Team">
+// <copyright file="ItemsResult.cs" company="GW2.Net Coding Team">
 //   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
-using System.Collections.Generic;
 using Newtonsoft.Json;
 
-namespace GW2DotNET.V1.Core.Items
+namespace GW2DotNET.V1.Core.ItemsInformation.Catalog
 {
     /// <summary>
-    /// Represents a response that is the result of an <see cref="ItemsRequest"/>.
+    /// Wraps a collection of items.
     /// </summary>
-    /// <remarks>
-    /// See <a href="http://wiki.guildwars2.com/wiki/API:1/items"/> for more information.
-    /// </remarks>
-    public class ItemsResponse : JsonObject
+    public class ItemsResult : JsonObject
     {
         /// <summary>
-        /// Gets or sets a list of all discovered item IDs.
+        /// Gets or sets a list of item IDs.
         /// </summary>
         [JsonProperty("items")]
-        public List<int> Items { get; set; }
+        public Catalog.Items Items { get; set; }
     }
 }
