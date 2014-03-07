1ec16ff697defaeb45983a876569e7b203ccbe9d
Steven Liekens <steven.liekens@gmail.com>
2014-03-07 15:39:52 +0000

    Updating 'ColourData'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET-Http;C34750

--- GW2.NET Tests/ColoursTests.cs
+++ GW2.NET Tests/ColoursTests.cs
@@ -7,15 +7,12 @@
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
 using GW2DotNET.V1;
 using GW2DotNET.V1.Infrastructure;
-using GW2DotNET.V1.Items.DataProviders;
-using GW2DotNET.V1.Items.Models;
 
 using NUnit.Framework;
 
@@ -44,100 +41,55 @@ namespace GW2DotNET_Tests
         [Test]
         public void CheckCache()
         {
-            ColourData colourData = this.manager.ColourData;
+            var colourData = this.manager.ColourData;
 
-            List<GwColour> list = colourData.ColourList.ToList();
+            var dyes = colourData.Dyes.ToList();
 
-            Assert.IsNotEmpty(list);
+            Assert.IsNotEmpty(dyes);
 
-            Trace.WriteLine(list.Count);
+            Trace.WriteLine(dyes.Count);
         }
 
         /// <summary>
         /// Gets all colours from the api.
         /// </summary>
         [Test]
-        public void GetColours()
+        public void GetDyes()
         {
-            Stopwatch stopwatch = Stopwatch.StartNew();
+            var stopwatch = Stopwatch.StartNew();
 
-            ColourData colourData = this.manager.ColourData;
+            var colourData = this.manager.ColourData;
 
-            List<GwColour> colours = colourData.GetColourList().ToList();
+            var dyes = colourData.GetDyes().ToList();
 
             stopwatch.Stop();
 
-            colourData.WriteCacheToDisk();
-
-            Assert.IsNotEmpty(colours);
+            Assert.IsNotEmpty(dyes);
 
             Trace.WriteLine(string.Format("Elapsed Time: {0}", stopwatch.ElapsedMilliseconds));
 
-            Trace.WriteLine(string.Format("Total colours: {0}", colours.Count));
+            Trace.WriteLine(string.Format("Total colours: {0}", dyes.Count));
         }
 
         /// <summary>
         /// Gets all colours asynchronously from the api.
         /// </summary>
         [Test]
-        public async Task GetAllColoursAsync()
-        {
-            Stopwatch stopwatch = Stopwatch.StartNew();
-
-            ColourData colourData = this.manager.ColourData;
-
-            IEnumerable<GwColour> coloursTask = await colourData.GetColourListAsync();
-
-            IEnumerable<GwColour> colours = coloursTask.ToList();
-
-            stopwatch.Stop();
-
-            Assert.IsNotEmpty(colours);
-
-            Trace.WriteLine(string.Format("Elapsed Time: {0}", stopwatch.ElapsedMilliseconds));
-
-            Trace.WriteLine(string.Format("Total colours: {0}", colours.Count()));
-        }
-
-        /// <summary>
-        /// Gets a single single colour by ID.
-        /// </summary>
-        [Test]
-        public void GetSingleColourFromId()
+        public async Task GetDyesAsync()
         {
             var stopwatch = Stopwatch.StartNew();
 
-            this.manager.ColourData.BypassCache = true;
-            GwColour colour = this.manager.ColourData.GetSingleColour(677);
+            var colourData = this.manager.ColourData;
+
+            var dyes = (await colourData.GetDyesAsync().ConfigureAwait(false)).ToList();
 
             stopwatch.Stop();
 
-            Assert.IsNotNullOrEmpty(colour.Name);
+            Assert.IsNotEmpty(dyes);
 
             Trace.WriteLine(string.Format("Elapsed Time: {0}", stopwatch.ElapsedMilliseconds));
 
-            Trace.WriteLine(string.Format("Single colour name: {0}", colour.Name));
-        }
-
-        /// <summary>The get colour from id asynchronously.</summary>
-        [Test]
-        public async Task GetColourFromIdAsync()
-        {
-            var stopwatch = Stopwatch.StartNew();
-
-            
-            var task = this.manager.ColourData.GetSingleColourAsync(1231);
-            task.Wait();
-
-            var result = await task;
-
-            stopwatch.Stop();
-
-            Assert.IsNotNullOrEmpty(result.Name);
-
-            Trace.WriteLine(string.Format("Elapsed Time: {0}", stopwatch.ElapsedMilliseconds));
-
-            Trace.WriteLine(string.Format("Single colour name: {0}", task.Result.Name));
+            Trace.WriteLine(string.Format("Total colours: {0}", dyes.Count()));
         }
     }
 }
--- GW2.NET/V1/Items/DataProviders/ColourData.cs
+++ GW2.NET/V1/Items/DataProviders/ColourData.cs
@@ -9,11 +9,10 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-
+using GW2DotNET.V1.Core.ColorsInformation.Details;
 using GW2DotNET.V1.Infrastructure;
-using GW2DotNET.V1.Items.Models;
+using GW2DotNET.V1.RestSharp;
 
 namespace GW2DotNET.V1.Items.DataProviders
 {
@@ -31,7 +30,7 @@ namespace GW2DotNET.V1.Items.DataProviders
         private readonly IDataManager dataManager;
 
         /// <summary>The colours cache.</summary>
-        private Lazy<List<GwColour>> coloursCache;
+        private Lazy<Dyes> coloursCache;
 
         // --------------------------------------------------------------------------------------------------------------------
         // Constructors & Destructors
@@ -55,7 +54,7 @@ namespace GW2DotNET.V1.Items.DataProviders
 
             int build;
 
-            this.coloursCache = !this.BypassCache ? new Lazy<List<GwColour>>(() => this.ReadCacheFromDisk<List<GwColour>>(this.colourDataCacheFileName, out build)) : new Lazy<List<GwColour>>();
+            this.coloursCache = !this.BypassCache ? new Lazy<Dyes>(() => this.ReadCacheFromDisk<Dyes>(this.colourDataCacheFileName, out build)) : new Lazy<Dyes>();
         }
 
         // --------------------------------------------------------------------------------------------------------------------
@@ -63,7 +62,7 @@ namespace GW2DotNET.V1.Items.DataProviders
         // --------------------------------------------------------------------------------------------------------------------
 
         /// <summary>Gets the colour list.</summary>
-        public IEnumerable<GwColour> ColourList
+        public IEnumerable<KeyValuePair<int, Dye>> Dyes
         {
             get
             {
@@ -78,9 +77,10 @@ namespace GW2DotNET.V1.Items.DataProviders
         /// <summary>Writes the complete cache to the disk using the specified serializer.</summary>
         public override void WriteCacheToDisk()
         {
-            var cache = new GameCache<List<GwColour>>
+            var cache = new GameCache<Dyes>
                         {
-                            Build = this.dataManager.Build, CacheData = this.coloursCache.Value
+                            Build = this.dataManager.Build,
+                            CacheData = this.coloursCache.Value
                         };
 
             this.WriteDataToDisk(this.colourDataCacheFileName, cache);
@@ -96,105 +96,47 @@ namespace GW2DotNET.V1.Items.DataProviders
         /// <summary>Clears the cache.</summary>
         public override void ClearCache()
         {
-            this.coloursCache = new Lazy<List<GwColour>>();
+            this.coloursCache = new Lazy<Dyes>();
         }
 
         /// <summary>Calls the GW2 api to get a list of all colours asynchronously.</summary>
         /// <returns>A <see cref="IEnumerable{T}" /> containing all colours in the game.</returns>
-        public async Task<IEnumerable<GwColour>> GetColourListAsync()
+        public async Task<IEnumerable<KeyValuePair<int, Dye>>> GetDyesAsync()
         {
-            var arguments = new List<KeyValuePair<string, object>>
-                            {
-                                new KeyValuePair<string, object>("lang", this.dataManager.Language)
-                            };
-
-            Dictionary<string, Dictionary<int, GwColour>> returnContent = await ApiCall.GetContentAsync<Dictionary<string, Dictionary<int, GwColour>>>("colors.json", arguments, ApiCall.Categories.Miscellaneous);
-
-            Dictionary<int, GwColour> coloursDictionary = returnContent["colors"];
-
-            List<GwColour> colours = this.ResolveColourId(coloursDictionary);
+            var serviceClient = ServiceClient.Create();
+            var request       = new ColorsRequest(); // TODO: CultureInfo parameter
+            var response      = await request.GetResponseAsync(serviceClient).ConfigureAwait(false);
+            var dyes          = response.EnsureSuccessStatusCode().Deserialize().Colors;
 
             if (!this.BypassCache)
             {
-                this.coloursCache.Value.AddRange(colours);
+                foreach (var kvp in dyes)
+                {
+                    this.coloursCache.Value[kvp.Key] = kvp.Value;
+                }
             }
 
-            return colours;
+            return dyes;
         }
 
         /// <summary>Calls the GW2 api to get a list of all colours synchronously.</summary>
         /// <returns>A <see cref="IEnumerable{T}" /> containing all colours in the game.</returns>
-        public IEnumerable<GwColour> GetColourList()
+        public IEnumerable<KeyValuePair<int, Dye>> GetDyes()
         {
-            var arguments = new List<KeyValuePair<string, object>>
-                            {
-                                new KeyValuePair<string, object>("lang", this.dataManager.Language)
-                            };
-
-            var returnContent = ApiCall.GetContent<Dictionary<string, Dictionary<int, GwColour>>>("colors.json", arguments, ApiCall.Categories.Miscellaneous);
-
-            Dictionary<int, GwColour> coloursDictionary = returnContent["colors"];
-
-            List<GwColour> colours = this.ResolveColourId(coloursDictionary);
+            var serviceClient = ServiceClient.Create();
+            var request       = new ColorsRequest(); // TODO: CultureInfo parameter
+            var response      = request.GetResponse(serviceClient);
+            var dyes          = response.EnsureSuccessStatusCode().Deserialize().Colors;
 
             if (!this.BypassCache)
             {
-                this.coloursCache.Value.AddRange(colours);
+                foreach (var kvp in dyes)
+                {
+                    this.coloursCache.Value[kvp.Key] = kvp.Value;
+                }
             }
 
-            return colours;
-        }
-
-        /// <summary>Checks if the requested colour is already in the cache, if not calls the GW2 api to get the specified colour asynchronously.</summary>
-        /// <param name="colourId">The colour id.</param>
-        /// <returns>The <see cref="GwColour"/> with the specified id.</returns>
-        public async Task<GwColour> GetSingleColourAsync(int colourId)
-        {
-            if (this.coloursCache.Value.Any(colour => colour.Id == colourId))
-            {
-                return this.coloursCache.Value.FirstOrDefault(colour => colour.Id == colourId);
-            }
-
-            GwColour colourToReturn = (await this.GetColourListAsync()).FirstOrDefault(colour => colour.Id == colourId);
-
-            if (!this.BypassCache)
-            {
-                this.coloursCache.Value.Add(colourToReturn);
-            }
-
-            return colourToReturn;
-        }
-
-        /// <summary>Checks if the requested colour is already in the cache, if not calls the GW2 api to get the specified colour synchronously.</summary>
-        /// <param name="colourId">The colour id.</param>
-        /// <returns>The <see cref="GwColour"/> with the specified id.</returns>
-        public GwColour GetSingleColour(int colourId)
-        {
-            if (this.coloursCache.Value != null && this.coloursCache.Value.Any(colour => colour.Id == colourId))
-            {
-                return this.coloursCache.Value.FirstOrDefault(colour => colour.Id == colourId);
-            }
-
-            GwColour colourToReturn = this.GetColourList().FirstOrDefault(colour => colour.Id == colourId);
-
-            if (!this.BypassCache)
-            {
-                this.coloursCache.Value.Add(colourToReturn);
-            }
-
-            return colourToReturn;
-        }
-
-        // --------------------------------------------------------------------------------------------------------------------
-        // Private Methods
-        // --------------------------------------------------------------------------------------------------------------------
-
-        /// <summary>Resolves the ids on a <see cref="Dictionary{TKey,TValue}"/> of colours.</summary>
-        /// <param name="coloursToResolve">The <see cref="Dictionary{TKey, TValue}"/> containing the colour ids as Key and the colours itself as value.</param>
-        /// <returns>The <see cref="List{T}"/> containing all colours with their ids resolved.</returns>
-        private List<GwColour> ResolveColourId(Dictionary<int, GwColour> coloursToResolve)
-        {
-            return coloursToResolve.Select(keyValuePair => new GwColour(keyValuePair.Key, keyValuePair.Value.Name, keyValuePair.Value.BaseRgb, keyValuePair.Value.Cloth, keyValuePair.Value.Leather, keyValuePair.Value.Metal)).ToList();
+            return dyes;
         }
     }
 }
