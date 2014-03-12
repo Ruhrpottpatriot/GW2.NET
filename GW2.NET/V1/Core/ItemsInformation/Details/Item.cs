c9a19b73e30d8a17b335d485ea97398ae910b230
Steven Liekens <steven.liekens@gmail.com>
2014-03-12 11:26:03 +0000

    Implementing equality and comparison members for 'Item' and 'Recipe'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET-Http;C34886

--- GW2.NET/V1/Core/ItemsInformation/Details/Item.cs
+++ GW2.NET/V1/Core/ItemsInformation/Details/Item.cs
@@ -8,6 +8,8 @@
 // --------------------------------------------------------------------------------------------------------------------
 namespace GW2DotNET.V1.Core.ItemsInformation.Details
 {
+    using System;
+
     using GW2DotNET.V1.Core.ItemsInformation.Details.Items;
 
     using Newtonsoft.Json;
@@ -16,7 +18,7 @@ namespace GW2DotNET.V1.Core.ItemsInformation.Details
     ///     Represents detailed information about an in-game item.
     /// </summary>
     [JsonConverter(typeof(ItemConverter))]
-    public abstract class Item : JsonObject
+    public abstract class Item : JsonObject, IEquatable<Item>, IComparable<Item>
     {
         /// <summary>Initializes a new instance of the <see cref="Item"/> class.</summary>
         /// <param name="type">The item's type.</param>
@@ -96,5 +98,98 @@ namespace GW2DotNET.V1.Core.ItemsInformation.Details
         /// </summary>
         [JsonProperty("vendor_value", Order = 6)]
         public int VendorValue { get; set; }
+
+        /// <summary>
+        ///     Indicates whether an object is equal to another object of the same type.
+        /// </summary>
+        /// <param name="left">The object on the left side.</param>
+        /// <param name="right">The object on the right side.</param>
+        /// <returns>
+        ///     true if the <paramref name="left" /> parameter is equal to the <paramref name="right" /> parameter; otherwise,
+        ///     false.
+        /// </returns>
+        public static bool operator ==(Item left, Item right)
+        {
+            return object.Equals(left, right);
+        }
+
+        /// <summary>
+        ///     Indicates whether an object differs from another object of the same type.
+        /// </summary>
+        /// <param name="left">The object on the left side.</param>
+        /// <param name="right">The object on the right side.</param>
+        /// <returns>
+        ///     true if the <paramref name="left" /> parameter differs from the <paramref name="right" /> parameter;
+        ///     otherwise, false.
+        /// </returns>
+        public static bool operator !=(Item left, Item right)
+        {
+            return !object.Equals(left, right);
+        }
+
+        /// <summary>Compares the current object with another object of the same type.</summary>
+        /// <returns>A value that indicates the relative order of the objects being compared. The return value has the following meanings: Value Meaning Less than zero This object is less than the <paramref name="other"/> parameter.Zero This object is equal to <paramref name="other"/>. Greater than zero This object is greater than <paramref name="other"/>. </returns>
+        /// <param name="other">An object to compare with this object.</param>
+        public int CompareTo(Item other)
+        {
+            if (other == null)
+            {
+                return 1;
+            }
+
+            return this.ItemId.CompareTo(other.ItemId);
+        }
+
+        /// <summary>Indicates whether the current object is equal to another object of the same type.</summary>
+        /// <returns>true if the current object is equal to the <paramref name="other"/> parameter; otherwise, false.</returns>
+        /// <param name="other">An object to compare with this object.</param>
+        public bool Equals(Item other)
+        {
+            if (object.ReferenceEquals(null, other))
+            {
+                return false;
+            }
+
+            if (object.ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return this.ItemId == other.ItemId;
+        }
+
+        /// <summary>Determines whether the specified <see cref="T:System.Object"/> is equal to the current <see cref="T:System.Object"/>.</summary>
+        /// <returns>true if the specified <see cref="T:System.Object"/> is equal to the current <see cref="T:System.Object"/>; otherwise, false.</returns>
+        /// <param name="obj">The <see cref="T:System.Object"/> to compare with the current <see cref="T:System.Object"/>. </param>
+        public override bool Equals(object obj)
+        {
+            if (object.ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (object.ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            return this.Equals((Item)obj);
+        }
+
+        /// <summary>
+        /// Serves as a hash function for a particular type. 
+        /// </summary>
+        /// <returns>
+        /// A hash code for the current <see cref="T:System.Object"/>.
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return this.ItemId;
+        }
     }
 }
