d4af4a78e78e6f9a4602c3ce4ed320f23000c1a4
Steven Liekens <steven.liekens@gmail.com>
2014-03-12 13:06:56 +0000

    Implementing equality and comparison members for 'Region', 'Subregion', 'PointOfInterest', 'RenownTask', 'Sector' and 'SkillChallenge'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET-Http;C34891

--- GW2.NET/V1/Core/MapsInformation/Floors/Regions/Subregions/Locations/RenownTask.cs
+++ GW2.NET/V1/Core/MapsInformation/Floors/Regions/Subregions/Locations/RenownTask.cs
@@ -8,6 +8,7 @@
 // --------------------------------------------------------------------------------------------------------------------
 namespace GW2DotNET.V1.Core.MapsInformation.Floors.Regions.Subregions.Locations
 {
+    using System;
     using System.Drawing;
 
     using GW2DotNET.V1.Core.Converters;
@@ -17,7 +18,7 @@ namespace GW2DotNET.V1.Core.MapsInformation.Floors.Regions.Subregions.Locations
     /// <summary>
     ///     Represents a renown heart location.
     /// </summary>
-    public class RenownTask : JsonObject
+    public class RenownTask : JsonObject, IEquatable<RenownTask>, IComparable<RenownTask>
     {
         /// <summary>
         ///     Gets or sets the task's coordinates.
@@ -43,5 +44,98 @@ namespace GW2DotNET.V1.Core.MapsInformation.Floors.Regions.Subregions.Locations
         /// </summary>
         [JsonProperty("task_id", Order = 0)]
         public int TaskId { get; set; }
+
+        /// <summary>
+        ///     Indicates whether an object is equal to another object of the same type.
+        /// </summary>
+        /// <param name="left">The object on the left side.</param>
+        /// <param name="right">The object on the right side.</param>
+        /// <returns>
+        ///     true if the <paramref name="left" /> parameter is equal to the <paramref name="right" /> parameter; otherwise,
+        ///     false.
+        /// </returns>
+        public static bool operator ==(RenownTask left, RenownTask right)
+        {
+            return object.Equals(left, right);
+        }
+
+        /// <summary>
+        ///     Indicates whether an object differs from another object of the same type.
+        /// </summary>
+        /// <param name="left">The object on the left side.</param>
+        /// <param name="right">The object on the right side.</param>
+        /// <returns>
+        ///     true if the <paramref name="left" /> parameter differs from the <paramref name="right" /> parameter;
+        ///     otherwise, false.
+        /// </returns>
+        public static bool operator !=(RenownTask left, RenownTask right)
+        {
+            return !object.Equals(left, right);
+        }
+
+        /// <summary>Compares the current object with another object of the same type.</summary>
+        /// <returns>A value that indicates the relative order of the objects being compared. The return value has the following meanings: Value Meaning Less than zero This object is less than the <paramref name="other"/> parameter.Zero This object is equal to <paramref name="other"/>. Greater than zero This object is greater than <paramref name="other"/>. </returns>
+        /// <param name="other">An object to compare with this object.</param>
+        public int CompareTo(RenownTask other)
+        {
+            if (other == null)
+            {
+                return 1;
+            }
+
+            return this.TaskId.CompareTo(other.TaskId);
+        }
+
+        /// <summary>Indicates whether the current object is equal to another object of the same type.</summary>
+        /// <returns>true if the current object is equal to the <paramref name="other"/> parameter; otherwise, false.</returns>
+        /// <param name="other">An object to compare with this object.</param>
+        public bool Equals(RenownTask other)
+        {
+            if (object.ReferenceEquals(null, other))
+            {
+                return false;
+            }
+
+            if (object.ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return this.TaskId == other.TaskId;
+        }
+
+        /// <summary>Determines whether the specified <see cref="T:System.Object"/> is equal to the current <see cref="T:System.Object"/>.</summary>
+        /// <returns>true if the specified <see cref="T:System.Object"/> is equal to the current <see cref="T:System.Object"/>; otherwise, false.</returns>
+        /// <param name="obj">The <see cref="T:System.Object"/> to compare with the current <see cref="T:System.Object"/>. </param>
+        public override bool Equals(object obj)
+        {
+            if (object.ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (object.ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            return this.Equals((RenownTask)obj);
+        }
+
+        /// <summary>
+        /// Serves as a hash function for a particular type. 
+        /// </summary>
+        /// <returns>
+        /// A hash code for the current <see cref="T:System.Object"/>.
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return this.TaskId;
+        }
     }
 }
