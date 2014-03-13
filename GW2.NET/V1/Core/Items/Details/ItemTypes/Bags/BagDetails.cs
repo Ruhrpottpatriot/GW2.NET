990be43e88796335231af0ba26aa82d71523eed6
Steven Liekens <steven.liekens@gmail.com>
2014-03-13 21:22:29 +0000

    Implementing comparison members for 'BagDetails'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET-Http;C34944

--- GW2.NET/V1/Core/Items/Details/ItemTypes/Bags/BagDetails.cs
+++ GW2.NET/V1/Core/Items/Details/ItemTypes/Bags/BagDetails.cs
@@ -8,6 +8,8 @@
 // --------------------------------------------------------------------------------------------------------------------
 namespace GW2DotNET.V1.Core.Items.Details.ItemTypes.Bags
 {
+    using System;
+
     using GW2DotNET.V1.Core.Common;
     using GW2DotNET.V1.Core.Common.Converters;
 
@@ -16,7 +18,7 @@ namespace GW2DotNET.V1.Core.Items.Details.ItemTypes.Bags
     /// <summary>
     ///     Represents detailed information about a bag.
     /// </summary>
-    public class BagDetails : JsonObject
+    public class BagDetails : JsonObject, IEquatable<BagDetails>
     {
         /// <summary>Gets or sets the bag.</summary>
         public Bag Bag { get; set; }
@@ -33,5 +35,85 @@ namespace GW2DotNET.V1.Core.Items.Details.ItemTypes.Bags
         /// </summary>
         [JsonProperty("size", Order = 1)]
         public int Size { get; set; }
+
+        /// <summary>
+        ///     Indicates whether an object is equal to another object of the same type.
+        /// </summary>
+        /// <param name="left">The object on the left side.</param>
+        /// <param name="right">The object on the right side.</param>
+        /// <returns>
+        ///     true if the <paramref name="left" /> parameter is equal to the <paramref name="right" /> parameter; otherwise,
+        ///     false.
+        /// </returns>
+        public static bool operator ==(BagDetails left, BagDetails right)
+        {
+            return object.Equals(left, right);
+        }
+
+        /// <summary>
+        ///     Indicates whether an object differs from another object of the same type.
+        /// </summary>
+        /// <param name="left">The object on the left side.</param>
+        /// <param name="right">The object on the right side.</param>
+        /// <returns>
+        ///     true if the <paramref name="left" /> parameter differs from the <paramref name="right" /> parameter;
+        ///     otherwise, false.
+        /// </returns>
+        public static bool operator !=(BagDetails left, BagDetails right)
+        {
+            return !object.Equals(left, right);
+        }
+
+        /// <summary>Indicates whether the current object is equal to another object of the same type.</summary>
+        /// <returns>true if the current object is equal to the <paramref name="other"/> parameter; otherwise, false.</returns>
+        /// <param name="other">An object to compare with this object.</param>
+        public bool Equals(BagDetails other)
+        {
+            if (object.ReferenceEquals(null, other))
+            {
+                return false;
+            }
+
+            if (object.ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return object.Equals(this.Bag, other.Bag);
+        }
+
+        /// <summary>Determines whether the specified <see cref="T:System.Object"/> is equal to the current <see cref="T:System.Object"/>.</summary>
+        /// <returns>true if the specified <see cref="T:System.Object"/> is equal to the current <see cref="T:System.Object"/>; otherwise, false.</returns>
+        /// <param name="obj">The object to compare with the current object. </param>
+        public override bool Equals(object obj)
+        {
+            if (object.ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (object.ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            return this.Equals((BagDetails)obj);
+        }
+
+        /// <summary>
+        /// Serves as a hash function for a particular type. 
+        /// </summary>
+        /// <returns>
+        /// A hash code for the current <see cref="T:System.Object"/>.
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return this.Bag != null ? this.Bag.GetHashCode() : 0;
+        }
     }
 }
