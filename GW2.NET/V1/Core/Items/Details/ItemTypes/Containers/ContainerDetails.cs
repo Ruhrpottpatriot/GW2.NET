92395d2ffb1525b5a3fcf5cb2ca54dd20b054ab8
Steven Liekens <steven.liekens@gmail.com>
2014-03-13 22:16:48 +0000

    Implementing comparison members for 'ContainerDetails'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET-Http;C34953

--- GW2.NET/V1/Core/Items/Details/ItemTypes/Containers/ContainerDetails.cs
+++ GW2.NET/V1/Core/Items/Details/ItemTypes/Containers/ContainerDetails.cs
@@ -8,6 +8,8 @@
 // --------------------------------------------------------------------------------------------------------------------
 namespace GW2DotNET.V1.Core.Items.Details.ItemTypes.Containers
 {
+    using System;
+
     using GW2DotNET.V1.Core.Common;
 
     using Newtonsoft.Json;
@@ -16,7 +18,7 @@ namespace GW2DotNET.V1.Core.Items.Details.ItemTypes.Containers
     ///     Represents detailed information about a container.
     /// </summary>
     [JsonConverter(typeof(ContainerDetailsConverter))]
-    public abstract class ContainerDetails : JsonObject
+    public abstract class ContainerDetails : JsonObject, IEquatable<ContainerDetails>
     {
         /// <summary>Initializes a new instance of the <see cref="ContainerDetails"/> class.</summary>
         /// <param name="containerType">The container type.</param>
@@ -33,5 +35,85 @@ namespace GW2DotNET.V1.Core.Items.Details.ItemTypes.Containers
         /// </summary>
         [JsonProperty("type", Order = 0)]
         public ContainerType Type { get; set; }
+
+        /// <summary>
+        ///     Indicates whether an object is equal to another object of the same type.
+        /// </summary>
+        /// <param name="left">The object on the left side.</param>
+        /// <param name="right">The object on the right side.</param>
+        /// <returns>
+        ///     true if the <paramref name="left" /> parameter is equal to the <paramref name="right" /> parameter; otherwise,
+        ///     false.
+        /// </returns>
+        public static bool operator ==(ContainerDetails left, ContainerDetails right)
+        {
+            return object.Equals(left, right);
+        }
+
+        /// <summary>
+        ///     Indicates whether an object differs from another object of the same type.
+        /// </summary>
+        /// <param name="left">The object on the left side.</param>
+        /// <param name="right">The object on the right side.</param>
+        /// <returns>
+        ///     true if the <paramref name="left" /> parameter differs from the <paramref name="right" /> parameter;
+        ///     otherwise, false.
+        /// </returns>
+        public static bool operator !=(ContainerDetails left, ContainerDetails right)
+        {
+            return !object.Equals(left, right);
+        }
+
+        /// <summary>Indicates whether the current object is equal to another object of the same type.</summary>
+        /// <returns>true if the current object is equal to the <paramref name="other"/> parameter; otherwise, false.</returns>
+        /// <param name="other">An object to compare with this object.</param>
+        public bool Equals(ContainerDetails other)
+        {
+            if (object.ReferenceEquals(null, other))
+            {
+                return false;
+            }
+
+            if (object.ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return object.Equals(this.Container, other.Container);
+        }
+
+        /// <summary>Determines whether the specified <see cref="T:System.Object"/> is equal to the current <see cref="T:System.Object"/>.</summary>
+        /// <returns>true if the specified <see cref="T:System.Object"/> is equal to the current <see cref="T:System.Object"/>; otherwise, false.</returns>
+        /// <param name="obj">The object to compare with the current object. </param>
+        public override bool Equals(object obj)
+        {
+            if (object.ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (object.ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            return this.Equals((ContainerDetails)obj);
+        }
+
+        /// <summary>
+        /// Serves as a hash function for a particular type. 
+        /// </summary>
+        /// <returns>
+        /// A hash code for the current <see cref="T:System.Object"/>.
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return this.Container != null ? this.Container.GetHashCode() : 0;
+        }
     }
 }
