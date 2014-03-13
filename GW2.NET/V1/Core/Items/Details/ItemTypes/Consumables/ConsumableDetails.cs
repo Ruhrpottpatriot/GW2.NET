ae9fcc41014f0b0ee2201e8a7cd317fe179cbfcc
Steven Liekens <steven.liekens@gmail.com>
2014-03-13 21:57:49 +0000

    Implementing comparison members for 'ConsumableDetails'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET-Http;C34947

--- GW2.NET/V1/Core/Items/Details/ItemTypes/Consumables/ConsumableDetails.cs
+++ GW2.NET/V1/Core/Items/Details/ItemTypes/Consumables/ConsumableDetails.cs
@@ -8,6 +8,8 @@
 // --------------------------------------------------------------------------------------------------------------------
 namespace GW2DotNET.V1.Core.Items.Details.ItemTypes.Consumables
 {
+    using System;
+
     using GW2DotNET.V1.Core.Common;
 
     using Newtonsoft.Json;
@@ -16,7 +18,7 @@ namespace GW2DotNET.V1.Core.Items.Details.ItemTypes.Consumables
     ///     Represents detailed information about a consumable item.
     /// </summary>
     [JsonConverter(typeof(ConsumableDetailsConverter))]
-    public abstract class ConsumableDetails : JsonObject
+    public abstract class ConsumableDetails : JsonObject, IEquatable<ConsumableDetails>
     {
         /// <summary>Initializes a new instance of the <see cref="ConsumableDetails"/> class.</summary>
         /// <param name="type">The consumable's type.</param>
@@ -33,5 +35,85 @@ namespace GW2DotNET.V1.Core.Items.Details.ItemTypes.Consumables
         /// </summary>
         [JsonProperty("type", Order = 0)]
         public ConsumableType Type { get; set; }
+
+        /// <summary>
+        ///     Indicates whether an object is equal to another object of the same type.
+        /// </summary>
+        /// <param name="left">The object on the left side.</param>
+        /// <param name="right">The object on the right side.</param>
+        /// <returns>
+        ///     true if the <paramref name="left" /> parameter is equal to the <paramref name="right" /> parameter; otherwise,
+        ///     false.
+        /// </returns>
+        public static bool operator ==(ConsumableDetails left, ConsumableDetails right)
+        {
+            return object.Equals(left, right);
+        }
+
+        /// <summary>
+        ///     Indicates whether an object differs from another object of the same type.
+        /// </summary>
+        /// <param name="left">The object on the left side.</param>
+        /// <param name="right">The object on the right side.</param>
+        /// <returns>
+        ///     true if the <paramref name="left" /> parameter differs from the <paramref name="right" /> parameter;
+        ///     otherwise, false.
+        /// </returns>
+        public static bool operator !=(ConsumableDetails left, ConsumableDetails right)
+        {
+            return !object.Equals(left, right);
+        }
+
+        /// <summary>Indicates whether the current object is equal to another object of the same type.</summary>
+        /// <returns>true if the current object is equal to the <paramref name="other"/> parameter; otherwise, false.</returns>
+        /// <param name="other">An object to compare with this object.</param>
+        public bool Equals(ConsumableDetails other)
+        {
+            if (object.ReferenceEquals(null, other))
+            {
+                return false;
+            }
+
+            if (object.ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return object.Equals(this.Consumable, other.Consumable);
+        }
+
+        /// <summary>Determines whether the specified <see cref="T:System.Object"/> is equal to the current <see cref="T:System.Object"/>.</summary>
+        /// <returns>true if the specified <see cref="T:System.Object"/> is equal to the current <see cref="T:System.Object"/>; otherwise, false.</returns>
+        /// <param name="obj">The object to compare with the current object. </param>
+        public override bool Equals(object obj)
+        {
+            if (object.ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (object.ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            return this.Equals((ConsumableDetails)obj);
+        }
+
+        /// <summary>
+        /// Serves as a hash function for a particular type. 
+        /// </summary>
+        /// <returns>
+        /// A hash code for the current <see cref="T:System.Object"/>.
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return this.Consumable != null ? this.Consumable.GetHashCode() : 0;
+        }
     }
 }
