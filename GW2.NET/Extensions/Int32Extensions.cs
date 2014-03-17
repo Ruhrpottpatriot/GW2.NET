b32d25b5c6a72e21ac53edb3c7cb4e4a51c3dc22
Steven Liekens <steven.liekens@gmail.com>
2014-03-17 18:47:53 +0000

    Adding 'ToStringInvariant()' extensions for 'int' and 'Nullable<int>' that get their string representation using the formatting rules of the invariant culture
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET-Http;C35049

---
+++ GW2.NET/Extensions/Int32Extensions.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Int32Extensions.cs" company="GW2.Net Coding Team">
+//   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+// <summary>
+//   Provides extension methods for the  type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace GW2DotNET.Extensions
+{
+    using System.Globalization;
+
+    /// <summary>Provides extension methods for the <see cref="T:System.Int32"/> type.</summary>
+    public static class Int32Extensions
+    {
+        /// <summary>Converts the numeric value of this instance to its equivalent string representation using the format rules of the invariant culture.</summary>
+        /// <param name="instance">The instance.</param>
+        /// <returns>The string representation of the value of this instance as specified by provider.</returns>
+        public static string ToStringInvariant(this int instance)
+        {
+            return instance.ToString(NumberFormatInfo.InvariantInfo);
+        }
+
+        /// <summary>Converts the numeric value of this instance to its equivalent string representation using the format rules of the invariant culture.</summary>
+        /// <param name="instance">The instance.</param>
+        /// <returns>The string representation of the value of this instance as specified by provider.</returns>
+        public static string ToStringInvariant(this int? instance)
+        {
+            return !instance.HasValue ? string.Empty : instance.Value.ToString(NumberFormatInfo.InvariantInfo);
+        }
+    }
+}
