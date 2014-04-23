8957ddc56f532f2f6ee9600c04bf2261573bcb98
Steven Liekens <steven.liekens@gmail.com>
2014-04-23 14:09:38 +0000

    Adding an enum member for 'ItemFlags.AccountBindOnUse'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET-Http;C35733

--- GW2.NET/V1/Items.Details/Types/ItemFlags.cs
+++ GW2.NET/V1/Items.Details/Types/ItemFlags.cs
@@ -21,46 +21,50 @@ namespace GW2DotNET.V1.Items.Details.Types
     public enum ItemFlags
     {
         /// <summary> Indicates no additional item flags.</summary>
-        None = 0, 
+        None = 0,
 
         /// <summary>The 'Account Bound' item flag.</summary>
         [EnumMember(Value = "AccountBound")]
-        AccountBound = 1 << 0, 
+        AccountBound = 1 << 0,
 
         /// <summary>The 'Hide Suffix' item flag.</summary>
         [EnumMember(Value = "HideSuffix")]
-        HideSuffix = 1 << 1, 
+        HideSuffix = 1 << 1,
 
         /// <summary>The 'No Mystic Forge' item flag.</summary>
         [EnumMember(Value = "NoMysticForge")]
-        NoMysticForge = 1 << 2, 
+        NoMysticForge = 1 << 2,
 
         /// <summary>The 'No Salvage' item flag.</summary>
         [EnumMember(Value = "NoSalvage")]
-        NoSalvage = 1 << 3, 
+        NoSalvage = 1 << 3,
 
         /// <summary>The 'No Sell' item flag.</summary>
         [EnumMember(Value = "NoSell")]
-        NoSell = 1 << 4, 
+        NoSell = 1 << 4,
 
         /// <summary>The 'Not Upgradeable' item flag.</summary>
         [EnumMember(Value = "NotUpgradeable")]
-        NotUpgradeable = 1 << 5, 
+        NotUpgradeable = 1 << 5,
 
         /// <summary>The 'No Underwater' item flag.</summary>
         [EnumMember(Value = "NoUnderwater")]
-        NoUnderwater = 1 << 6, 
+        NoUnderwater = 1 << 6,
 
-        /// <summary>The 'SoulBind On Acquire' item flag.</summary>
+        /// <summary>The 'Soul Bind On Acquire' item flag.</summary>
         [EnumMember(Value = "SoulBindOnAcquire")]
-        SoulBindOnAcquire = 1 << 7, 
+        SoulBindOnAcquire = 1 << 7,
 
-        /// <summary>The 'SoulBind On Use' item flag.</summary>
+        /// <summary>The 'Soul Bind On Use' item flag.</summary>
         [EnumMember(Value = "SoulBindOnUse")]
-        SoulBindOnUse = 1 << 8, 
+        SoulBindOnUse = 1 << 8,
 
         /// <summary>The 'Unique' item flag.</summary>
         [EnumMember(Value = "Unique")]
-        Unique = 1 << 9
+        Unique = 1 << 9,
+
+        /// <summary>The 'Account Bind On Use' item flag.</summary>
+        [EnumMember(Value = "AccountBindOnUse")]
+        AccountBindOnUse = 1 << 10
     }
 }
