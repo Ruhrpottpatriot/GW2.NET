2db514214df5ab8bda6f62bde44a66b846a51c05
Steven Liekens <steven.liekens@gmail.com>
2014-05-09 13:32:58 +0000

    Converting 'NourishmentDetails' to an abstract class
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET-Http;C36067

--- GW2.NET/V1/Items.Details.Contracts/ItemTypes/Consumables/ConsumableTypes/NourishmentDetails.cs
+++ GW2.NET/V1/Items.Details.Contracts/ItemTypes/Consumables/ConsumableTypes/NourishmentDetails.cs
@@ -3,7 +3,7 @@
 //   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
 // </copyright>
 // <summary>
-//   Represents detailed information about a nourishment item.
+//   Provides the base class for nourishment details.
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 namespace GW2DotNET.V1.Items.Details.Contracts.ItemTypes.Consumables.ConsumableTypes
@@ -15,13 +15,13 @@ namespace GW2DotNET.V1.Items.Details.Contracts.ItemTypes.Consumables.ConsumableT
 
     using Newtonsoft.Json;
 
-    /// <summary>Represents detailed information about a nourishment item.</summary>
+    /// <summary>Provides the base class for nourishment details.</summary>
     [JsonConverter(typeof(DefaultJsonConverter))]
-    public class NourishmentDetails : ConsumableDetails
+    public abstract class NourishmentDetails : ConsumableDetails
     {
         /// <summary>Initializes a new instance of the <see cref="NourishmentDetails"/> class.</summary>
         /// <param name="type">The consumable's type.</param>
-        public NourishmentDetails(ConsumableType type)
+        protected NourishmentDetails(ConsumableType type)
             : base(type)
         {
         }
