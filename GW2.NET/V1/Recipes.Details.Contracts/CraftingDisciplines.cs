94fafbe9a983f45956f8403e64bef80560b34090
Steven Liekens <steven.liekens@gmail.com>
2014-06-03 08:58:21 +0000

    Adding a flag for 'CraftingDisciplines.None'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET-Http;C36505

--- GW2.NET/V1/Recipes.Details.Contracts/CraftingDisciplines.cs
+++ GW2.NET/V1/Recipes.Details.Contracts/CraftingDisciplines.cs
@@ -3,7 +3,7 @@
 //   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
 // </copyright>
 // <summary>
-//   Enumerates the possible crafting disciplines.
+//   Enumerates the known crafting disciplines.
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
@@ -16,38 +16,41 @@ namespace GW2DotNET.V1.Recipes.Details.Contracts
 
     using Newtonsoft.Json;
 
-    /// <summary>Enumerates the possible crafting disciplines.</summary>
+    /// <summary>Enumerates the known crafting disciplines.</summary>
     [Flags]
     [JsonConverter(typeof(StringEnumFlagsConverter))]
     public enum CraftingDisciplines
     {
+        /// <summary>Indicates no crafting disciplines.</summary>
+        None = 0,
+
         /// <summary>The 'Armor smith' crafting discipline.</summary>
         [EnumMember(Value = "Armorsmith")]
-        Armorsmith = 1 << 0, 
+        Armorsmith = 1 << 0,
 
         /// <summary>The 'Artificer' crafting discipline.</summary>
         [EnumMember(Value = "Artificer")]
-        Artificer = 1 << 1, 
+        Artificer = 1 << 1,
 
         /// <summary>The 'Chef' crafting discipline.</summary>
         [EnumMember(Value = "Chef")]
-        Chef = 1 << 2, 
+        Chef = 1 << 2,
 
         /// <summary>The 'Huntsman' crafting discipline.</summary>
         [EnumMember(Value = "Huntsman")]
-        Huntsman = 1 << 3, 
+        Huntsman = 1 << 3,
 
         /// <summary>The 'Jeweler' crafting discipline.</summary>
         [EnumMember(Value = "Jeweler")]
-        Jeweler = 1 << 4, 
+        Jeweler = 1 << 4,
 
         /// <summary>The 'Leatherworker' crafting discipline.</summary>
         [EnumMember(Value = "Leatherworker")]
-        Leatherworker = 1 << 5, 
+        Leatherworker = 1 << 5,
 
         /// <summary>The 'Tailor' crafting discipline.</summary>
         [EnumMember(Value = "Tailor")]
-        Tailor = 1 << 6, 
+        Tailor = 1 << 6,
 
         /// <summary>The 'Weapon smith' crafting discipline.</summary>
         [EnumMember(Value = "Weaponsmith")]
