67e21f79f377b969e5cc9320f2c2155d01612803
Steven Liekens <steven.liekens@gmail.com>
2014-06-18 13:18:03 +0000

    Adding a stub for 'IServiceClient'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET-Http;C36740

---
+++ GW2.NET/V2/Common/IServiceClient.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IServiceClient.cs" company="GW2.NET Coding Team">
+//   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+// <summary>
+//   Provides the interface for service clients.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace GW2DotNET.V2.Common
+{
+    /// <summary>Provides the interface for service clients.</summary>
+    public interface IServiceClient
+    {
+    }
+}
