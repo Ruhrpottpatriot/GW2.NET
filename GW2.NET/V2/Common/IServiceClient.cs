71f65020cb3f214b3532a33a27f76520ede1a7c7
Steven Liekens <steven.liekens@gmail.com>
2014-06-18 13:29:43 +0000

    Adding synchronous 'Send' methods to 'IServiceClient'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET-Http;C36741

--- GW2.NET/V2/Common/IServiceClient.cs
+++ GW2.NET/V2/Common/IServiceClient.cs
@@ -6,11 +6,39 @@
 //   Provides the interface for service clients.
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
-
 namespace GW2DotNET.V2.Common
 {
+    using System.Collections.Generic;
+    using System.Globalization;
+
     /// <summary>Provides the interface for service clients.</summary>
     public interface IServiceClient
     {
+        /// <summary>Sends a request and returns the response.</summary>
+        /// <param name="request">The service request.</param>
+        /// <typeparam name="TResult">The type of the response content.</typeparam>
+        /// <returns>A collection of the specified type.</returns>
+        IEnumerable<TResult> Send<TResult>(IRequest request);
+
+        /// <summary>Sends a request and returns the response.</summary>
+        /// <param name="request">The service request.</param>
+        /// <param name="culture">The culture.</param>
+        /// <typeparam name="TResult">The type of the response content.</typeparam>
+        /// <returns>An instance of the specified type.</returns>
+        TResult Send<TResult>(IDetailsRequest request, CultureInfo culture = null);
+
+        /// <summary>Sends a request and returns the response.</summary>
+        /// <param name="request">The service request.</param>
+        /// <param name="culture">The culture.</param>
+        /// <typeparam name="TResult">The type of the response content.</typeparam>
+        /// <returns>A collection of the specified type.</returns>
+        IEnumerable<TResult> Send<TResult>(IBulkRequest request, CultureInfo culture = null);
+
+        /// <summary>Sends a request and returns the response.</summary>
+        /// <param name="request">The service request.</param>
+        /// <param name="culture">The culture.</param>
+        /// <typeparam name="TResult">The type of the response content.</typeparam>
+        /// <returns>A collection of the specified type.</returns>
+        IEnumerable<TResult> Send<TResult>(IPaginatedRequest request, CultureInfo culture = null);
     }
 }
