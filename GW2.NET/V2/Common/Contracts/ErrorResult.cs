9caf5ecbefd51b98f62eefb3ec22f3acda35d00c
Steven Liekens <steven.liekens@gmail.com>
2014-06-18 11:52:03 +0000

    Adding class 'ErrorResult'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET-Http;C36735

---
+++ GW2.NET/V2/Common/Contracts/ErrorResult.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ErrorResult.cs" company="GW2.NET Coding Team">
+//   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+// <summary>
+//   Represents the result that is returned when an error occurs.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace GW2DotNET.V2.Common.Contracts
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    /// <summary>Represents the result that is returned when an error occurs.</summary>
+    [Serializable]
+    public class ErrorResult
+    {
+        /// <summary>Gets or sets the error message.</summary>
+        [DataMember(Name = "text")]
+        public string Text { get; set; }
+    }
+}
