bcd5bd5b936e552f18367f3cbc948525464a6a0d
Steven Liekens <steven.liekens@gmail.com>
2014-06-19 10:23:16 +0000

    - Renaming package 'RestSharp.GW2.NET' to 'GW2.NET.RestSharp' - Bringing version numbers up to 0.9.3
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET-Http;C36762

--- GW2.NET.RestSharp/Properties/AssemblyInfo.cs
+++ GW2.NET.RestSharp/Properties/AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RestSharp.GW2.NET")]
+[assembly: AssemblyTitle("GW2.NET.RestSharp")]
 [assembly: AssemblyDescription("A RestSharp-specific implementation for GW2.NET")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GW2.NET Coding Team")]
-[assembly: AssemblyProduct("RestSharp.GW2.NET")]
+[assembly: AssemblyProduct("GW2.NET.RestSharp")]
 [assembly: AssemblyCopyright("Copyright ©  2013 - 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -38,5 +38,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.0.0")]
-[assembly: AssemblyFileVersion("0.9.0.0")]
+[assembly: AssemblyVersion("0.9.3.0")]
+[assembly: AssemblyFileVersion("0.9.3.0")]
