687cee3260775d7aa4562915032cc9a63164ea1e
Steven Liekens <steven.liekens@gmail.com>
2014-06-21 14:38:24 +0000

    Implementing initial support for '/v2/build'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET-V2;C36806

--- GW2.NET/V2/Builds/BuildService.cs
+++ GW2.NET/V2/Builds/BuildService.cs
@@ -1,28 +1,44 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="ServiceManager.cs" company="GW2.NET Coding Team">
+// <copyright file="BuildService.cs" company="GW2.NET Coding Team">
 //   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
 // </copyright>
 // <summary>
-//   Provides access to the Guild Wars 2 service.
+//   Provides the default implementation of the build service.
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
-namespace GW2DotNET.V2
+namespace GW2DotNET.V2.Builds
 {
     using System.Threading;
     using System.Threading.Tasks;
 
-    using GW2DotNET.V2.Builds;
+    using GW2DotNET.V1.Common;
     using GW2DotNET.V2.Builds.Contracts;
 
-    /// <summary>Provides access to the Guild Wars 2 service.</summary>
-    public class ServiceManager : IBuildService
+    /// <summary>Provides the default implementation of the build service.</summary>
+    public class BuildService : IBuildService
     {
+        /// <summary>Infrastructure. Holds a reference to the service client.</summary>
+        private readonly IServiceClient serviceClient;
+
+        /// <summary>Initializes a new instance of the <see cref="BuildService" /> class.</summary>
+        public BuildService()
+            : this(new ServiceClient())
+        {
+        }
+
+        /// <summary>Initializes a new instance of the <see cref="BuildService"/> class.</summary>
+        /// <param name="serviceClient">The service client.</param>
+        public BuildService(IServiceClient serviceClient)
+        {
+            this.serviceClient = serviceClient;
+        }
+
         /// <summary>Gets the current game build.</summary>
         /// <returns>The current game build.</returns>
         /// <remarks>See <a href="http://wiki.guildwars2.com/wiki/API:1/build">wiki</a> for more information.</remarks>
         public Build GetBuild()
         {
-            return new BuildService().GetBuild();
+            return this.serviceClient.Send<Build>(new BuildRequest());
         }
 
         /// <summary>Gets the current build.</summary>
@@ -30,7 +46,7 @@ namespace GW2DotNET.V2
         /// <remarks>See <a href="http://wiki.guildwars2.com/wiki/API:1/build">wiki</a> for more information.</remarks>
         public Task<Build> GetBuildAsync()
         {
-            return new BuildService().GetBuildAsync(CancellationToken.None);
+            return this.GetBuildAsync(CancellationToken.None);
         }
 
         /// <summary>Gets the current build.</summary>
@@ -39,7 +55,7 @@ namespace GW2DotNET.V2
         /// <remarks>See <a href="http://wiki.guildwars2.com/wiki/API:1/build">wiki</a> for more information.</remarks>
         public Task<Build> GetBuildAsync(CancellationToken cancellationToken)
         {
-            return new BuildService().GetBuildAsync(cancellationToken);
+            return this.serviceClient.SendAsync<Build>(new BuildRequest(), cancellationToken);
         }
     }
 }
--- GW2.NET/V2/Builds/IBuildService.cs
+++ GW2.NET/V2/Builds/IBuildService.cs
@@ -1,45 +1,35 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="ServiceManager.cs" company="GW2.NET Coding Team">
+// <copyright file="IBuildService.cs" company="GW2.NET Coding Team">
 //   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
 // </copyright>
 // <summary>
-//   Provides access to the Guild Wars 2 service.
+//   Provides the interface for the build service.
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
-namespace GW2DotNET.V2
+namespace GW2DotNET.V2.Builds
 {
     using System.Threading;
     using System.Threading.Tasks;
 
-    using GW2DotNET.V2.Builds;
     using GW2DotNET.V2.Builds.Contracts;
 
-    /// <summary>Provides access to the Guild Wars 2 service.</summary>
-    public class ServiceManager : IBuildService
+    /// <summary>Provides the interface for the build service.</summary>
+    public interface IBuildService
     {
         /// <summary>Gets the current game build.</summary>
         /// <returns>The current game build.</returns>
         /// <remarks>See <a href="http://wiki.guildwars2.com/wiki/API:1/build">wiki</a> for more information.</remarks>
-        public Build GetBuild()
-        {
-            return new BuildService().GetBuild();
-        }
+        Build GetBuild();
 
         /// <summary>Gets the current build.</summary>
         /// <returns>The current game build.</returns>
         /// <remarks>See <a href="http://wiki.guildwars2.com/wiki/API:1/build">wiki</a> for more information.</remarks>
-        public Task<Build> GetBuildAsync()
-        {
-            return new BuildService().GetBuildAsync(CancellationToken.None);
-        }
+        Task<Build> GetBuildAsync();
 
         /// <summary>Gets the current build.</summary>
         /// <param name="cancellationToken">The <see cref="CancellationToken"/> that provides cancellation support.</param>
         /// <returns>The current game build.</returns>
         /// <remarks>See <a href="http://wiki.guildwars2.com/wiki/API:1/build">wiki</a> for more information.</remarks>
-        public Task<Build> GetBuildAsync(CancellationToken cancellationToken)
-        {
-            return new BuildService().GetBuildAsync(cancellationToken);
-        }
+        Task<Build> GetBuildAsync(CancellationToken cancellationToken);
     }
 }
