49fbed7a3df3323e3de1efdc7bc6f5fad7566383
Steven Liekens <steven.liekens@gmail.com>
2014-06-24 11:33:26 +0000

    Adding a reusable [TypeDiscriminatorAttribute]
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET;C36830

---
+++ GW2.NET/Common/TypeDiscriminatorAttribute.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TypeDiscriminatorAttribute.cs" company="GW2.NET Coding Team">
+//   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+// <summary>
+//   The type discriminator attribute.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace GW2DotNET.Common
+{
+    using System;
+
+    /// <summary>The type discriminator attribute.</summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class TypeDiscriminatorAttribute : Attribute
+    {
+        /// <summary>Gets or sets the name.</summary>
+        public string Value { get; set; }
+
+        /// <summary>Gets or sets the base type.</summary>
+        public Type BaseType { get; set; }
+    }
+}
