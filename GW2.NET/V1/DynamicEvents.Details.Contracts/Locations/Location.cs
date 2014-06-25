7b9dc6270e82b3ba99c9e65bb541151564bd1192
Steven Liekens <steven.liekens@gmail.com>
2014-06-25 12:57:12 +0000

    Removing 'IEquatable' implementation from 'Location'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET;C36881

--- GW2.NET/V1/DynamicEvents.Details.Contracts/Locations/Location.cs
+++ GW2.NET/V1/DynamicEvents.Details.Contracts/Locations/Location.cs
@@ -15,7 +15,7 @@ namespace GW2DotNET.V1.DynamicEvents.Details.Contracts.Locations
     using GW2DotNET.V1.Common.Drawing;
 
     /// <summary>Represents the location of an event on the map.</summary>
-    public abstract class Location : JsonObject, IEquatable<Location>
+    public abstract class Location : JsonObject
     {
         /// <summary>Gets or sets the center coordinates.</summary>
         [DataMember(Name = "center", Order = 1)]
@@ -23,71 +23,5 @@ namespace GW2DotNET.V1.DynamicEvents.Details.Contracts.Locations
 
         /// <summary>Gets or sets the dynamic event details.</summary>
         public DynamicEventDetails DynamicEventDetails { get; set; }
-
-        /// <summary>Indicates whether an object is equal to another object of the same type.</summary>
-        /// <param name="left">The object on the left side.</param>
-        /// <param name="right">The object on the right side.</param>
-        /// <returns>true if the <paramref name="left" /> parameter is equal to the <paramref name="right" /> parameter; otherwise, false.</returns>
-        public static bool operator ==(Location left, Location right)
-        {
-            return object.Equals(left, right);
-        }
-
-        /// <summary>Indicates whether an object differs from another object of the same type.</summary>
-        /// <param name="left">The object on the left side.</param>
-        /// <param name="right">The object on the right side.</param>
-        /// <returns>true if the <paramref name="left" /> parameter differs from the <paramref name="right" /> parameter; otherwise, false.</returns>
-        public static bool operator !=(Location left, Location right)
-        {
-            return !object.Equals(left, right);
-        }
-
-        /// <summary>Indicates whether the current object is equal to another object of the same type.</summary>
-        /// <returns>true if the current object is equal to the <paramref name="other"/> parameter; otherwise, false.</returns>
-        /// <param name="other">An object to compare with this object.</param>
-        public bool Equals(Location other)
-        {
-            if (object.ReferenceEquals(null, other))
-            {
-                return false;
-            }
-
-            if (object.ReferenceEquals(this, other))
-            {
-                return true;
-            }
-
-            return this.Center.Equals(other.Center);
-        }
-
-        /// <summary>Determines whether the specified <see cref="T:System.Object"/> is equal to the current<see cref="T:System.Object"/>.</summary>
-        /// <returns>true if the specified <see cref="T:System.Object"/> is equal to the current <see cref="T:System.Object"/>; otherwise, false.</returns>
-        /// <param name="obj">The <see cref="T:System.Object"/> to compare with the current <see cref="T:System.Object"/>. </param>
-        public override bool Equals(object obj)
-        {
-            if (object.ReferenceEquals(null, obj))
-            {
-                return false;
-            }
-
-            if (object.ReferenceEquals(this, obj))
-            {
-                return true;
-            }
-
-            if (obj.GetType() != this.GetType())
-            {
-                return false;
-            }
-
-            return this.Equals((Location)obj);
-        }
-
-        /// <summary>Serves as a hash function for a particular type.</summary>
-        /// <returns>A hash code for the current <see cref="T:System.Object" />.</returns>
-        public override int GetHashCode()
-        {
-            return this.Center.GetHashCode();
-        }
     }
 }
