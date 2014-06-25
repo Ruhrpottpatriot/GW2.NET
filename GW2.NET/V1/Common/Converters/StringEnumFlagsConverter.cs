72ce6c4a3fec55e3724e8083e4a85cbf123dc9ed
Steven Liekens <steven.liekens@gmail.com>
2014-06-25 21:01:11 +0000

    Improving the implementation of 'StringEnumFlagsConverter'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET;C36898

--- GW2.NET/V1/Common/Converters/StringEnumFlagsConverter.cs
+++ GW2.NET/V1/Common/Converters/StringEnumFlagsConverter.cs
@@ -3,28 +3,47 @@
 //   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
 // </copyright>
 // <summary>
-//   Converts a bit flag <see cref="Enum" /> to and from a string array of flag names.
+//   Converts a bit flag <see cref="Enum" /> to and from a JSON string array.
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 namespace GW2DotNET.V1.Common.Converters
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
 
     using Newtonsoft.Json;
     using Newtonsoft.Json.Converters;
     using Newtonsoft.Json.Linq;
 
-    /// <summary>Converts a bit flag <see cref="Enum" /> to and from a string array of flag names.</summary>
-    public class StringEnumFlagsConverter : StringEnumConverter
+    /// <summary>Converts a bit flag <see cref="Enum" /> to and from a JSON string array.</summary>
+    public sealed class StringEnumFlagsConverter : StringEnumConverter
     {
+        /// <summary>Gets a value indicating whether this <see cref="T:Newtonsoft.Json.JsonConverter"/> can read JSON.</summary>
+        /// <value><c>true</c> if this <see cref="T:Newtonsoft.Json.JsonConverter"/> can read JSON; otherwise, <c>false</c>.</value>
+        public override bool CanRead
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        /// <summary>Gets a value indicating whether this <see cref="T:Newtonsoft.Json.JsonConverter"/> can write JSON.</summary>
+        /// <value><c>true</c> if this <see cref="T:Newtonsoft.Json.JsonConverter"/> can write JSON; otherwise, <c>false</c>.</value>
+        public override bool CanWrite
+        {
+            get
+            {
+                return true;
+            }
+        }
+
         /// <summary>Determines whether this instance can convert the specified object type.</summary>
         /// <param name="objectType">Type of the object.</param>
         /// <returns>Returns <c>true</c> if this instance can convert the specified object type; otherwise <c>false</c>.</returns>
         public override bool CanConvert(Type objectType)
         {
-            return objectType.IsEnum;
+            return objectType.IsEnum && objectType.GetCustomAttributes(typeof(FlagsAttribute), false).Any();
         }
 
         /// <summary>Reads the JSON representation of the object.</summary>
@@ -35,33 +54,17 @@ namespace GW2DotNET.V1.Common.Converters
         /// <returns>The object value.</returns>
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
-            if (reader.TokenType != JsonToken.StartArray)
+            var values = serializer.Deserialize<string[]>(reader);
+            if (values == null || !values.Any())
             {
-                return base.ReadJson(reader, objectType, existingValue, serializer);
+                return existingValue;
             }
 
-            var individualFlags = new Stack<string>();
-            do
-            {
-                if (reader.TokenType == JsonToken.String)
-                {
-                    individualFlags.Push(reader.Value.ToString());
-                }
-            }
-            while (reader.Read() && reader.TokenType != JsonToken.EndArray);
-
-            if (individualFlags.Any())
-            {
-                var flags = string.Join(",", individualFlags);
-                var jsonFlags = JValue.CreateString(flags);
-                var valueReader = jsonFlags.CreateReader();
-                if (valueReader.Read())
-                {
-                    return base.ReadJson(valueReader, objectType, existingValue, serializer);
-                }
-            }
-
-            return existingValue;
+            var flags = string.Join(",", values);
+            var jsonFlags = JValue.CreateString(flags);
+            var valueReader = jsonFlags.CreateReader();
+            valueReader.Read();
+            return base.ReadJson(valueReader, objectType, existingValue, serializer);
         }
 
         /// <summary>Writes the JSON representation of the object.</summary>
@@ -72,7 +75,8 @@ namespace GW2DotNET.V1.Common.Converters
         {
             if (value == null || (int)value == 0)
             {
-                WriteEmptyArray(writer);
+                writer.WriteStartArray();
+                writer.WriteEndArray();
                 return;
             }
 
@@ -84,14 +88,6 @@ namespace GW2DotNET.V1.Common.Converters
                 base.WriteJson(writer, Enum.Parse(enumType, item, true), serializer);
             }
 
-            writer.WriteEndArray();
-        }
-
-        /// <summary>Writes an opening and closing bracket.</summary>
-        /// <param name="writer">The <see cref="JsonWriter"/> to write to.</param>
-        private static void WriteEmptyArray(JsonWriter writer)
-        {
-            writer.WriteStartArray();
             writer.WriteEndArray();
         }
     }
