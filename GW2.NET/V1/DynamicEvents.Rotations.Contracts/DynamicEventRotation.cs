827d57b1ce992514c6a9e0dea75ab284d28da1d8
Steven Liekens <steven.liekens@gmail.com>
2014-06-26 08:53:39 +0000

    Adding 'IEquatable' implementation for 'DynamicEventRotation'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET;C36913

--- GW2.NET/V1/DynamicEvents.Rotations.Contracts/DynamicEventRotation.cs
+++ GW2.NET/V1/DynamicEvents.Rotations.Contracts/DynamicEventRotation.cs
@@ -14,7 +14,7 @@ namespace GW2DotNET.V1.DynamicEvents.Rotations.Contracts
     using GW2DotNET.Common.Contracts;
 
     /// <summary>Represents a dynamic event and its rotation.</summary>
-    public class DynamicEventRotation : JsonObject
+    public class DynamicEventRotation : JsonObject, IEquatable<DynamicEventRotation>
     {
         /// <summary>Gets or sets the event identifier.</summary>
         [DataMember(Name = "event_id")]
@@ -23,5 +23,71 @@ namespace GW2DotNET.V1.DynamicEvents.Rotations.Contracts
         /// <summary>Gets or sets the event shifts.</summary>
         [DataMember(Name = "shifts")]
         public DynamicEventShifts Shifts { get; set; }
+
+        /// <summary>Indicates whether an object is equal to another object of the same type.</summary>
+        /// <param name="left">The object on the left side.</param>
+        /// <param name="right">The object on the right side.</param>
+        /// <returns>true if the <paramref name="left" /> parameter is equal to the <paramref name="right" /> parameter; otherwise, false.</returns>
+        public static bool operator ==(DynamicEventRotation left, DynamicEventRotation right)
+        {
+            return object.Equals(left, right);
+        }
+
+        /// <summary>Indicates whether an object differs from another object of the same type.</summary>
+        /// <param name="left">The object on the left side.</param>
+        /// <param name="right">The object on the right side.</param>
+        /// <returns>true if the <paramref name="left" /> parameter differs from the <paramref name="right" /> parameter; otherwise, false.</returns>
+        public static bool operator !=(DynamicEventRotation left, DynamicEventRotation right)
+        {
+            return !object.Equals(left, right);
+        }
+
+        /// <summary>Indicates whether the current object is equal to another object of the same type.</summary>
+        /// <returns>true if the current object is equal to the <paramref name="other"/> parameter; otherwise, false.</returns>
+        /// <param name="other">An object to compare with this object.</param>
+        public bool Equals(DynamicEventRotation other)
+        {
+            if (object.ReferenceEquals(null, other))
+            {
+                return false;
+            }
+
+            if (object.ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return this.EventId.Equals(other.EventId);
+        }
+
+        /// <summary>Determines whether the specified <see cref="T:System.Object"/> is equal to the current <see cref="T:System.Object"/>.</summary>
+        /// <returns>true if the specified <see cref="T:System.Object"/> is equal to the current <see cref="T:System.Object"/>; otherwise, false.</returns>
+        /// <param name="obj">The object to compare with the current object. </param>
+        public override bool Equals(object obj)
+        {
+            if (object.ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (object.ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            return this.Equals((DynamicEventRotation)obj);
+        }
+
+        /// <summary>Serves as a hash function for a particular type.</summary>
+        /// <returns>A hash code for the current <see cref="T:System.Object" />.</returns>
+        public override int GetHashCode()
+        {
+            return this.EventId.GetHashCode();
+        }
     }
 }
