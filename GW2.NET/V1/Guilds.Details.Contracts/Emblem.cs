38a41972ced1eaae61785ee8311aa131b4cb240e
Steven Liekens <steven.liekens@gmail.com>
2014-06-26 09:12:18 +0000

    Implementing the associations between 'Emblem' and 'ColorPalette'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET;C36918

--- GW2.NET/V1/Guilds.Details.Contracts/Emblem.cs
+++ GW2.NET/V1/Guilds.Details.Contracts/Emblem.cs
@@ -11,31 +11,41 @@ namespace GW2DotNET.V1.Guilds.Details.Contracts
     using System.Runtime.Serialization;
 
     using GW2DotNET.Common.Contracts;
+    using GW2DotNET.V1.Colors.Contracts;
 
     /// <summary>Represents a guild's emblem.</summary>
     public class Emblem : JsonObject
     {
-        /// <summary>Gets or sets the background color ID.</summary>
+        /// <summary>Gets or sets the background color.</summary>
+        public ColorPalette BackgroundColor { get; set; }
+
+        /// <summary>Gets or sets the background color identifier.</summary>
         [DataMember(Name = "background_color_id")]
         public int BackgroundColorId { get; set; }
 
-        /// <summary>Gets or sets the background image ID.</summary>
+        /// <summary>Gets or sets the background image identifier.</summary>
         [DataMember(Name = "background_id")]
         public int BackgroundId { get; set; }
 
-        /// <summary>Gets or sets the image transformations, if any.</summary>
+        /// <summary>Gets or sets the image transformations.</summary>
         [DataMember(Name = "flags")]
         public EmblemTransformations Flags { get; set; }
 
-        /// <summary>Gets or sets the foreground image ID.</summary>
+        /// <summary>Gets or sets the foreground image identifier.</summary>
         [DataMember(Name = "foreground_id")]
         public int ForegroundId { get; set; }
 
-        /// <summary>Gets or sets the primary foreground color ID.</summary>
+        /// <summary>Gets or sets the primary foreground color.</summary>
+        public ColorPalette ForegroundPrimaryColor { get; set; }
+
+        /// <summary>Gets or sets the primary foreground color identifier.</summary>
         [DataMember(Name = "foreground_primary_color_id")]
         public int ForegroundPrimaryColorId { get; set; }
 
-        /// <summary>Gets or sets the secondary foreground color ID.</summary>
+        /// <summary>Gets or sets the secondary foreground color.</summary>
+        public ColorPalette ForegroundSecondaryColor { get; set; }
+
+        /// <summary>Gets or sets the secondary foreground color identifier.</summary>
         [DataMember(Name = "foreground_secondary_color_id")]
         public int ForegroundSecondaryColorId { get; set; }
     }
