66fa92b7fd2ef12863e20293f3a9a77073a24c98
Steven Liekens <steven.liekens@gmail.com>
2014-06-26 08:48:32 +0000

    Removing 'IComparable' implementation from 'DynamicEvent'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET;C36910

--- GW2.NET/V1/DynamicEvents.Contracts/DynamicEvent.cs
+++ GW2.NET/V1/DynamicEvents.Contracts/DynamicEvent.cs
@@ -17,7 +17,7 @@ namespace GW2DotNET.V1.DynamicEvents.Contracts
     using Newtonsoft.Json.Converters;
 
     /// <summary>Represents a dynamic event and its status.</summary>
-    public class DynamicEvent : JsonObject, IEquatable<DynamicEvent>, IComparable<DynamicEvent>
+    public class DynamicEvent : JsonObject, IEquatable<DynamicEvent>
     {
         /// <summary>Gets or sets the <see cref="Guid" /> identifying the event.</summary>
         [DataMember(Name = "event_id")]
@@ -52,19 +52,6 @@ namespace GW2DotNET.V1.DynamicEvents.Contracts
         public static bool operator !=(DynamicEvent left, DynamicEvent right)
         {
             return !object.Equals(left, right);
-        }
-
-        /// <summary>Compares the current object with another object of the same type.</summary>
-        /// <returns>A value that indicates the relative order of the objects being compared. The return value has the following meanings: Value Meaning Less than zero This object is less than the <paramref name="other"/> parameter.Zero This object is equal to <paramref name="other"/>. Greater than zero This object is greater than<paramref name="other"/>.</returns>
-        /// <param name="other">An object to compare with this object.</param>
-        public int CompareTo(DynamicEvent other)
-        {
-            if (other == null)
-            {
-                return 1;
-            }
-
-            return this.EventId.CompareTo(other.EventId);
         }
 
         /// <summary>Indicates whether the current object is equal to another object of the same type.</summary>
