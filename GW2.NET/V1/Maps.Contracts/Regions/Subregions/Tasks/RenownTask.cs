7886aac412233bb87bfe9ef0f814b9c04900a45a
Steven Liekens <steven.liekens@gmail.com>
2014-06-27 19:45:06 +0000

    Overriding 'RenownTask.ToString()'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET;C36997

--- GW2.NET/V1/Maps.Contracts/Regions/Subregions/Tasks/RenownTask.cs
+++ GW2.NET/V1/Maps.Contracts/Regions/Subregions/Tasks/RenownTask.cs
@@ -10,6 +10,7 @@ namespace GW2DotNET.V1.Maps.Contracts.Regions.Subregions.Tasks
 {
     using System;
     using System.Drawing;
+    using System.Globalization;
     using System.Runtime.Serialization;
 
     using GW2DotNET.Common.Contracts;
@@ -97,6 +98,19 @@ namespace GW2DotNET.V1.Maps.Contracts.Regions.Subregions.Tasks
         public override int GetHashCode()
         {
             return this.TaskId;
+        }
+
+        /// <summary>Returns a string that represents the current object.</summary>
+        /// <returns>A string that represents the current object.</returns>
+        public override string ToString()
+        {
+            var name = this.Objective;
+            if (name != null)
+            {
+                return name;
+            }
+
+            return this.TaskId.ToString(NumberFormatInfo.InvariantInfo);
         }
     }
 }
