0c82fbbc14a6b18691bdc6ae3909da2919859357
Steven Liekens <steven.liekens@gmail.com>
2014-06-27 17:16:14 +0000

    Improving implementation of colors in 'Emblem'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET;C36975

--- GW2.NET/V1/Guilds.Details.Contracts/Emblem.cs
+++ GW2.NET/V1/Guilds.Details.Contracts/Emblem.cs
@@ -12,16 +12,17 @@ namespace GW2DotNET.V1.Guilds.Details.Contracts
 
     using GW2DotNET.Common.Contracts;
     using GW2DotNET.V1.Colors.Contracts;
+    using GW2DotNET.V1.Common.Converters;
+
+    using Newtonsoft.Json;
 
     /// <summary>Represents a guild's emblem.</summary>
     public class Emblem : ServiceContract
     {
         /// <summary>Gets or sets the background color.</summary>
-        public ColorPalette BackgroundColor { get; set; }
-
-        /// <summary>Gets or sets the background color identifier.</summary>
         [DataMember(Name = "background_color_id")]
-        public int BackgroundColorId { get; set; }
+        [JsonConverter(typeof(UnknownColorPaletteConverter))]
+        public ColorPalette BackgroundColor { get; set; }
 
         /// <summary>Gets or sets the background image identifier.</summary>
         [DataMember(Name = "background_id")]
@@ -36,17 +37,13 @@ namespace GW2DotNET.V1.Guilds.Details.Contracts
         public int ForegroundId { get; set; }
 
         /// <summary>Gets or sets the primary foreground color.</summary>
+        [DataMember(Name = "foreground_primary_color_id")]
+        [JsonConverter(typeof(UnknownColorPaletteConverter))]
         public ColorPalette ForegroundPrimaryColor { get; set; }
 
-        /// <summary>Gets or sets the primary foreground color identifier.</summary>
-        [DataMember(Name = "foreground_primary_color_id")]
-        public int ForegroundPrimaryColorId { get; set; }
-
         /// <summary>Gets or sets the secondary foreground color.</summary>
-        public ColorPalette ForegroundSecondaryColor { get; set; }
-
-        /// <summary>Gets or sets the secondary foreground color identifier.</summary>
         [DataMember(Name = "foreground_secondary_color_id")]
-        public int ForegroundSecondaryColorId { get; set; }
+        [JsonConverter(typeof(UnknownColorPaletteConverter))]
+        public ColorPalette ForegroundSecondaryColor { get; set; }
     }
 }
