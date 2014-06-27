11e436c794f89523136dc41132236ddcca4ddca7
Steven Liekens <steven.liekens@gmail.com>
2014-06-27 17:06:17 +0000

    Overriding 'ColorPalette.ToString()'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET;C36973

--- GW2.NET/V1/Colors.Contracts/ColorPalette.cs
+++ GW2.NET/V1/Colors.Contracts/ColorPalette.cs
@@ -10,6 +10,7 @@ namespace GW2DotNET.V1.Colors.Contracts
 {
     using System;
     using System.Drawing;
+    using System.Globalization;
     using System.Runtime.Serialization;
 
     using GW2DotNET.Common.Contracts;
@@ -113,6 +114,19 @@ namespace GW2DotNET.V1.Colors.Contracts
         public override int GetHashCode()
         {
             return this.ColorId;
+        }
+
+        /// <summary>Returns a string that represents the current object.</summary>
+        /// <returns>A string that represents the current object.</returns>
+        public override string ToString()
+        {
+            var name = this.Name;
+            if (name != null)
+            {
+                return name;
+            }
+
+            return this.ColorId.ToString(NumberFormatInfo.InvariantInfo);
         }
     }
 }
