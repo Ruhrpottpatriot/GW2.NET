bad6012b1f3918ce635dc4cd63311fdf3fb025c2
Steven Liekens <steven.liekens@gmail.com>
2014-06-27 14:40:26 +0000

    Overriding 'UnknownItem.ToString()'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET;C36955

--- GW2.NET/V1/Items.Details.Contracts/UnknownItem.cs
+++ GW2.NET/V1/Items.Details.Contracts/UnknownItem.cs
@@ -8,11 +8,25 @@
 // --------------------------------------------------------------------------------------------------------------------
 namespace GW2DotNET.V1.Items.Details.Contracts
 {
+    using System.Globalization;
+
     using GW2DotNET.Common;
 
     /// <summary>Represents an unknown item..</summary>
     [TypeDiscriminator(Value = "Unknown", BaseType = typeof(Item))]
     public class UnknownItem : Item
     {
+        /// <summary>Returns a string that represents the current object.</summary>
+        /// <returns>A string that represents the current object.</returns>
+        public override string ToString()
+        {
+            var name = this.Name;
+            if (!string.IsNullOrEmpty(name))
+            {
+                return name;
+            }
+
+            return this.ItemId.ToString(NumberFormatInfo.InvariantInfo);
+        }
     }
 }
