54f1caa9d507d4a1082a041d40c6ef77e955ce00
Steven Liekens <steven.liekens@gmail.com>
2014-06-28 16:56:14 +0000

    Changing 'SkinCollection' to derive from 'ServiceContractList'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET;C37027

--- GW2.NET/V1/Skins.Contracts/SkinCollection.cs
+++ GW2.NET/V1/Skins.Contracts/SkinCollection.cs
@@ -11,6 +11,7 @@ namespace GW2DotNET.V1.Skins.Contracts
     using System.Collections.Generic;
     using System.Runtime.Serialization;
 
+    using GW2DotNET.Common.Contracts;
     using GW2DotNET.V1.Common.Converters;
 
     using Newtonsoft.Json;
@@ -18,7 +19,7 @@ namespace GW2DotNET.V1.Skins.Contracts
     /// <summary>Represents a collection of skin identifiers.</summary>
     [CollectionDataContract]
     [JsonArray(ItemConverterType = typeof(UnknownSkinConverter))]
-    public class SkinCollection : List<Skin>
+    public class SkinCollection : ServiceContractList<Skin>
     {
         /// <summary>Initializes a new instance of the <see cref="SkinCollection" /> class.</summary>
         public SkinCollection()
