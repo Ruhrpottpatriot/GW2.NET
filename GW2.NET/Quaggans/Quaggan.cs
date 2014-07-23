f240f20c681a9290ec279bce8334a7446affbba2
Steven Liekens <steven.liekens@gmail.com>
2014-07-23 19:44:59 +0000

    * Adding entity classes that implement the relationships between various services * Changing data contracts to match the official JSON schemas * Changing the return type of services that return collections ** Collections of primitive types are returned as 'ICollection<T>' ** Collections of complex types are returned as 'IDictionary<TKey, TValue>' * Adding support for custom HTTP headers by re-introducing response wrappers (see 'IResponse')
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET-V2;C37215

---
+++ GW2.NET/Quaggans/Quaggan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GW2DotNET.Quaggans
+{
+    public class Quaggan
+    {
+        public string Identifier { get; set; }
+
+        public Uri Url { get; set; }
+    }
+}
---
+++ GW2.NET/V2/Quaggans.Contracts/QuagganContract.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GW2DotNET.V2.Quaggans.Contracts
+{
+    public class QuagganContract
+    {
+    }
+}
