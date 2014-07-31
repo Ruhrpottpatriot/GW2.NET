fa244ae0c2e25d3333bc6cfc34e2364a0083c5ff
Steven Liekens <steven.liekens@gmail.com>
2014-07-31 13:23:07 +0000

    Restoring code that went missing after the previous commit (wtf?)
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET;C37284

--- GW2.NET/V1/Skins/SkinDetailsServiceContract.cs
+++ GW2.NET/V1/Skins/SkinDetailsServiceContract.cs
@@ -3,7 +3,86 @@
 //   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
 // </copyright>
 // <summary>
-//   SkinDetailsServiceContract.cs
+//   The skin details service contracts.
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
+namespace GW2DotNET.V1.Skins
+{
+    using System.Diagnostics.Contracts;
+    using System.Globalization;
+    using System.Threading;
+    using System.Threading.Tasks;
 
+    using GW2DotNET.Skins;
+
+    /// <summary>The skin details service contract.</summary>
+    [ContractClassFor(typeof(ISkinDetailsService))]
+    internal abstract class SkinDetailsServiceContract : ISkinDetailsService
+    {
+        /// <summary>Gets a skin and its localized details.</summary>
+        /// <param name="skin">The skin identifier.</param>
+        /// <returns>A skin and its localized details.</returns>
+        /// <remarks>See <a href="http://wiki.guildwars2.com/wiki/API:1/skin_details">wiki</a> for more information.</remarks>
+        public Skin GetSkinDetails(int skin)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>Gets a skin and its localized details.</summary>
+        /// <param name="skin">The skin identifier.</param>
+        /// <param name="language">The language.</param>
+        /// <returns>A skin and its localized details.</returns>
+        /// <remarks>See <a href="http://wiki.guildwars2.com/wiki/API:1/skin_details">wiki</a> for more information.</remarks>
+        public Skin GetSkinDetails(int skin, CultureInfo language)
+        {
+            Contract.Requires(language != null);
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>Gets a skin and its localized details.</summary>
+        /// <param name="skin">The skin identifier.</param>
+        /// <returns>A skin and its localized details.</returns>
+        /// <remarks>See <a href="http://wiki.guildwars2.com/wiki/API:1/skin_details">wiki</a> for more information.</remarks>
+        public Task<Skin> GetSkinDetailsAsync(int skin)
+        {
+            Contract.Ensures(Contract.Result<Task<Skin>>() != null);
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>Gets a skin and its localized details.</summary>
+        /// <param name="skin">The skin identifier.</param>
+        /// <param name="language">The language.</param>
+        /// <returns>A skin and its localized details.</returns>
+        /// <remarks>See <a href="http://wiki.guildwars2.com/wiki/API:1/skin_details">wiki</a> for more information.</remarks>
+        public Task<Skin> GetSkinDetailsAsync(int skin, CultureInfo language)
+        {
+            Contract.Requires(language != null);
+            Contract.Ensures(Contract.Result<Task<Skin>>() != null);
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>Gets a skin and its localized details.</summary>
+        /// <param name="skin">The skin identifier.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> that provides cancellation support.</param>
+        /// <returns>A skin and its localized details.</returns>
+        /// <remarks>See <a href="http://wiki.guildwars2.com/wiki/API:1/skin_details">wiki</a> for more information.</remarks>
+        public Task<Skin> GetSkinDetailsAsync(int skin, CancellationToken cancellationToken)
+        {
+            Contract.Ensures(Contract.Result<Task<Skin>>() != null);
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>Gets a skin and its localized details.</summary>
+        /// <param name="skin">The skin identifier.</param>
+        /// <param name="language">The language.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> that provides cancellation support.</param>
+        /// <returns>A skin and its localized details.</returns>
+        /// <remarks>See <a href="http://wiki.guildwars2.com/wiki/API:1/skin_details">wiki</a> for more information.</remarks>
+        public Task<Skin> GetSkinDetailsAsync(int skin, CultureInfo language, CancellationToken cancellationToken)
+        {
+            Contract.Requires(language != null);
+            Contract.Ensures(Contract.Result<Task<Skin>>() != null);
+            throw new System.NotImplementedException();
+        }
+    }
+}
