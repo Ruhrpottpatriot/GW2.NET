723970cec8bba5288ad3f763a78c89da9537965b
Steven Liekens <steven.liekens@gmail.com>
2014-08-02 14:22:09 +0000

    Adding a reusable class 'DataContractSerializer'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET;C37336

---
+++ GW2.NET/Common/Serializers/DataContractSerializer.cs
@@ -0,0 +1,66 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DataContractSerializer.cs" company="GW2.NET Coding Team">
+//   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+// <summary>
+//   Provides methods for serializing data contracts.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace GW2DotNET.Common.Serializers
+{
+    using System.IO;
+    using System.Runtime.Serialization;
+    using System.Runtime.Serialization.Json;
+
+    /// <summary>Provides methods for serializing data contracts.</summary>
+    /// <typeparam name="T">The type of the data contract.</typeparam>
+    public class DataContractSerializer<T> : ISerializer<T>
+    {
+        /// <summary>Infrastructure. Holds a reference to the JSON.NET serializer.</summary>
+        private readonly XmlObjectSerializer serializer;
+
+        /// <summary>Initializes a new instance of the <see cref="DataContractSerializer{T}"/> class.</summary>
+        /// <param name="serializer">The serializer.</param>
+        public DataContractSerializer(XmlObjectSerializer serializer)
+        {
+            this.serializer = serializer;
+        }
+
+        /// <summary>Factory method. Creates a new instance of the <see cref="DataContractSerializer{T}"/> class and configures it for serializing JSON.</summary>
+        /// <returns>A new instance of the <see cref="DataContractSerializer{T}"/> class.</returns>
+        public static DataContractSerializer<T> ForJson()
+        {
+            return new DataContractSerializer<T>(new DataContractJsonSerializer(typeof(T)));
+        }
+
+        /// <summary>Factory method. Creates a new instance of the <see cref="DataContractSerializer{T}"/> class and configures it for serializing XML.</summary>
+        /// <returns>A new instance of the <see cref="DataContractSerializer{T}"/> class.</returns>
+        public static DataContractSerializer<T> ForXml()
+        {
+            return new DataContractSerializer<T>(new DataContractSerializer(typeof(T)));
+        }
+
+        /// <summary>Converts the input stream to the specified type.</summary>
+        /// <param name="stream">The input stream.</param>
+        /// <returns>An instance of the specified type.</returns>
+        public T Deserialize(Stream stream)
+        {
+            using (stream)
+            {
+                return (T)this.serializer.ReadObject(stream);
+            }
+        }
+
+        /// <summary>Converts the specified value to an output stream.</summary>
+        /// <param name="value">An instance of the specified type.</param>
+        /// <param name="stream">The output stream.</param>
+        public void Serialize(T value, Stream stream)
+        {
+            using (stream)
+            {
+                this.serializer.WriteObject(stream, value);
+            }
+        }
+    }
+}
