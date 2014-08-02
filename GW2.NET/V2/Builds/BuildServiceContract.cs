cf084193b63549268fa5ce10198b40baad9bb0d1
Steven Liekens <steven.liekens@gmail.com>
2014-08-02 15:03:59 +0000

    Adding a missing file header
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET;C37338

--- GW2.NET/V2/Builds/BuildServiceContract.cs
+++ GW2.NET/V2/Builds/BuildServiceContract.cs
@@ -1,4 +1,13 @@
-﻿namespace GW2DotNET.V2.Builds
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BuildServiceContract.cs" company="GW2.NET Coding Team">
+//   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+// <summary>
+//   The build service contract.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace GW2DotNET.V2.Builds
 {
     using System.Diagnostics.Contracts;
     using System.Threading;
