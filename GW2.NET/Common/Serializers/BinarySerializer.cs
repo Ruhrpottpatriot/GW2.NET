132bb6d413f453c141aab8a5e155ecfbc276d3b9
Steven Liekens <steven.liekens@gmail.com>
2014-08-09 14:44:20 +0000

    Adding a reusable 'BinarySerializer' and 'BinarySerializerFactory'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET;C37470

---
+++ GW2.NET/Common/Serializers/BinarySerializer.cs
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BinarySerializer.cs" company="GW2.NET Coding Team">
+//   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+// <summary>
+//   Provides methods for serializing binary data.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace GW2DotNET.Common.Serializers
+{
+    using System.IO;
+
+    /// <summary>Provides methods for serializing binary data.</summary>
+    public class BinarySerializer : ISerializer<byte[]>
+    {
+        /// <summary>Converts the input stream to the specified type.</summary>
+        /// <param name="stream">The input stream.</param>
+        /// <returns>An instance of the specified type.</returns>
+        public byte[] Deserialize(Stream stream)
+        {
+            // Create a buffer (64KiB)
+            var buffer = new byte[0xFFFF];
+
+            // Create a memory stream
+            using (var memoryStream = new MemoryStream())
+            {
+                // Copy all input data to memory
+                int count;
+                while (0 < (count = stream.Read(buffer, 0, buffer.Length)))
+                {
+                    memoryStream.Write(buffer, 0, count);
+                }
+
+                // Convert to a byte array and return
+                return memoryStream.ToArray();
+            }
+        }
+
+        /// <summary>Converts the specified value to an output stream.</summary>
+        /// <param name="value">An instance of the specified type.</param>
+        /// <param name="stream">The output stream.</param>
+        public void Serialize(byte[] value, Stream stream)
+        {
+            // Copy all data to the output stream
+            using (stream)
+            {
+                stream.Write(value, 0, value.Length);
+            }
+        }
+    }
+}
