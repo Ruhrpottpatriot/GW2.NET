e6d3207654123210d35257c638bd357dbb214eb0
Steven Liekens <steven.liekens@gmail.com>
2014-08-10 16:23:26 +0000

    Adding character encoding settings to 'StringSerializerFactory'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET;C37488

--- GW2.NET/Common/Serializers/StringSerializerFactory.cs
+++ GW2.NET/Common/Serializers/StringSerializerFactory.cs
@@ -9,10 +9,29 @@
 namespace GW2DotNET.Common.Serializers
 {
     using System;
+    using System.Diagnostics.Contracts;
+    using System.Text;
 
     /// <summary>Provides factory methods for the string serialization engine.</summary>
     public class StringSerializerFactory : ISerializerFactory
     {
+        /// <summary>Infrastructure. Holds a reference to the character encoding.</summary>
+        private readonly Encoding encoding;
+
+        /// <summary>Initializes a new instance of the <see cref="StringSerializerFactory"/> class.</summary>
+        public StringSerializerFactory()
+        {
+            this.encoding = Encoding.UTF8;
+        }
+
+        /// <summary>Initializes a new instance of the <see cref="StringSerializerFactory"/> class.</summary>
+        /// <param name="encoding">The character encoding.</param>
+        public StringSerializerFactory(Encoding encoding)
+        {
+            Contract.Requires(encoding != null);
+            this.encoding = encoding;
+        }
+
         /// <summary>Gets a serializer for the specified type.</summary>
         /// <typeparam name="T">The serialization type.</typeparam>
         /// <returns>The <see cref="ISerializer{T}"/>.</returns>
@@ -23,7 +42,14 @@ namespace GW2DotNET.Common.Serializers
                 throw new NotSupportedException("The specified type is not supported by the string serializer.");
             }
 
-            return (ISerializer<T>)new StringSerializer();
+            return (ISerializer<T>)new StringSerializer(this.encoding);
+        }
+
+        /// <summary>The invariant method for this class.</summary>
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this.encoding != null);
         }
     }
 }
