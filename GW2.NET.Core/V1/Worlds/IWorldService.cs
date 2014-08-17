6ba5924a86f8ab4e7c7e0082b81c37805a0a1e3c
Steven Liekens <steven.liekens@gmail.com>
2014-08-17 12:02:47 +0000

    IWorldService : adding missing file header
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET;C37618

--- GW2.NET.Core/V1/Worlds/IWorldService.cs
+++ GW2.NET.Core/V1/Worlds/IWorldService.cs
@@ -1,7 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IWorldService.cs" company="GW2.NET Coding Team">
+//   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+// <summary>
+//   Provides the interface for the world service.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace GW2DotNET.V1.Worlds
 {
