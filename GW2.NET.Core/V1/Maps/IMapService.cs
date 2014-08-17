96d9f1a86b70275f07388e8a6cd0d3562f43ec0d
Steven Liekens <steven.liekens@gmail.com>
2014-08-17 11:21:52 +0000

    Adding interface 'IMapService' as an aggregate interface for 'IMapNameService', 'IMapDetailsService',  'IMapFloorService' and 'IContinentDetailsService'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET;C37602

---
+++ GW2.NET.Core/V1/Maps/IMapService.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IMapService.cs" company="GW2.NET Coding Team">
+//   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+// <summary>
+//   Provides the interface for the maps service.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace GW2DotNET.V1.Maps
+{
+    /// <summary>Provides the interface for the maps service.</summary>
+    public interface IMapService : IMapNameService, IMapDetailsService, IMapFloorService, IContinentDetailsService
+    {
+    }
+}
