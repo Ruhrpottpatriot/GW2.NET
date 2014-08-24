c12a9d53e6cfe907a9b9c017170260471ac661ad
Steven Liekens <steven.liekens@gmail.com>
2014-08-24 11:09:07 +0000

    Adding cmdlet 'Get-Build'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET;C37737

---
+++ Code/GW2.NET.PowerShell/ServiceCmdlet.cs
@@ -0,0 +1,62 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ServiceCmdlet.cs" company="GW2.NET Coding Team">
+//   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+// <summary>
+//   Provides the base class for cmdlets that depend on a remote service.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace GW2DotNET.PS.Commands
+{
+    using System;
+    using System.Management.Automation;
+    using System.Threading;
+
+    using GW2DotNET.Common;
+    using GW2DotNET.Common.Serializers;
+
+    /// <summary>Provides the base class for cmdlets that depend on a remote service.</summary>
+    public abstract class ServiceCmdlet : Cmdlet
+    {
+        /// <summary>The cancellation token source.</summary>
+        private readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+        /// <summary>Gets the <see cref="CancellationToken"/> that provides cancellation support.</summary>
+        public CancellationToken CancellationToken
+        {
+            get
+            {
+                return this.cancellationTokenSource.Token;
+            }
+        }
+
+        /// <summary>Provides a one-time, preprocessing functionality for the cmdlet.</summary>
+        protected override sealed void BeginProcessing()
+        {
+            var baseUrl = new Uri("https://api.guildwars2.com", UriKind.Absolute);
+            var serializerFactory = new DataContractJsonSerializerFactory();
+            var serviceClient = new ServiceClient(baseUrl, serializerFactory, serializerFactory);
+            this.BeginProcessing(serviceClient);
+            base.BeginProcessing();
+        }
+
+        /// <summary>Provides a one-time, preprocessing functionality for the cmdlet.</summary>
+        /// <param name="serviceClient">A service client.</param>
+        protected virtual void BeginProcessing(IServiceClient serviceClient)
+        {
+        }
+
+        /// <summary>Provides a record-by-record processing functionality for the cmdlet.</summary>
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+        }
+
+        /// <summary>Stops processing records when the user stops the cmdlet asynchronously.</summary>
+        protected override sealed void StopProcessing()
+        {
+            this.cancellationTokenSource.Cancel();
+            base.StopProcessing();
+        }
+    }
+}
