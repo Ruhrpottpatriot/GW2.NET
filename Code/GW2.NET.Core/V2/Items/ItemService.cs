34fd3dfe637e04eb75fcc6fa1e856b4139f29b07
Steven Liekens <steven.liekens@gmail.com>
2014-09-25 19:48:56 +0000

    Implementing 'Discover()' and 'DiscoverAsync()' for /v2/items
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET;C38278

--- Code/GW2.NET.Core/V2/Items/ItemService.cs
+++ Code/GW2.NET.Core/V2/Items/ItemService.cs
@@ -1,10 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ItemService.cs" company="GW2.NET Coding Team">
+//   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+// <summary>
+//   Provides access to the /v2/items service using the default implementation.
+//   This implementation does not retrieve associated entities. For example: item skins can be retrieved from the skins service, but this class does not ever touch the skins service.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 namespace GW2DotNET.V2.Items
 {
+    using System;
+    using System.Collections.Generic;
     using System.Threading;
     using System.Threading.Tasks;
 
@@ -12,6 +18,10 @@ namespace GW2DotNET.V2.Items
     using GW2DotNET.Entities.Items;
     using GW2DotNET.V2.Common;
 
+    /// <summary>
+    /// Provides access to the /v2/items service using the default implementation.
+    /// This implementation does not retrieve associated entities. For example: item skins can be retrieved from the skins service, but this class does not ever touch the skins service.
+    /// </summary>
     public class ItemService : IRepository<int, Item>
     {
         /// <summary>Infrastructure. Holds a reference to the service client.</summary>
@@ -28,14 +38,21 @@ namespace GW2DotNET.V2.Items
         /// <returns>A collection of discovered identifiers.</returns>
         public ICollection<int> Discover()
         {
-            throw new NotImplementedException();
+            var request = new ItemDiscoveryRequest();
+            var response = this.serviceClient.Send<ICollection<int>>(request);
+            if (response.Content == null)
+            {
+                return null;
+            }
+
+            return response.Content;
         }
 
         /// <summary>Gets the discovered identifiers.</summary>
         /// <returns>A collection of discovered identifiers.</returns>
         public Task<ICollection<int>> DiscoverAsync()
         {
-            throw new NotImplementedException();
+            return this.DiscoverAsync(CancellationToken.None);
         }
 
         /// <summary>Gets the discovered identifiers.</summary>
@@ -43,60 +60,19 @@ namespace GW2DotNET.V2.Items
         /// <returns>A collection of discovered identifiers.</returns>
         public Task<ICollection<int>> DiscoverAsync(CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
-        }
+            var request = new ItemDiscoveryRequest();
+            return this.serviceClient.SendAsync<ICollection<int>>(request, cancellationToken).ContinueWith(
+                task =>
+                    {
+                        var response = task.Result;
+                        if (response.Content == null)
+                        {
+                            return new int[0];
+                        }
 
-        /// <summary>Gets a page with the specified page number.</summary>
-        /// <param name="page">The page to get.</param>
-        /// <returns>The page.</returns>
-        public ICollectionPage<Item> GetPage(int page)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>Gets a page with the specified page number and maximum size.</summary>
-        /// <param name="page">The page to get.</param>
-        /// <param name="pageSize">The maximum number of page elements.</param>
-        /// <returns>The page.</returns>
-        public ICollectionPage<Item> GetPage(int page, int pageSize)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>Gets a page with the specified page number.</summary>
-        /// <param name="page">The page to get.</param>
-        /// <returns>The page.</returns>
-        public Task<ICollectionPage<Item>> GetPageAsync(int page)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>Gets a page with the specified page number.</summary>
-        /// <param name="page">The page to get.</param>
-        /// <param name="cancellationToken">The <see cref="CancellationToken"/> that provides cancellation support.</param>
-        /// <returns>The page.</returns>
-        public Task<ICollectionPage<Item>> GetPageAsync(int page, CancellationToken cancellationToken)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>Gets a page with the specified page number.</summary>
-        /// <param name="page">The page to get.</param>
-        /// <param name="pageSize">The maximum number of page elements.</param>
-        /// <returns>The page.</returns>
-        public Task<ICollectionPage<Item>> GetPageAsync(int page, int pageSize)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>Gets a page with the specified page number.</summary>
-        /// <param name="page">The page to get.</param>
-        /// <param name="pageSize">The maximum number of page elements.</param>
-        /// <param name="cancellationToken">The <see cref="CancellationToken"/> that provides cancellation support.</param>
-        /// <returns>The page.</returns>
-        public Task<ICollectionPage<Item>> GetPageAsync(int page, int pageSize, CancellationToken cancellationToken)
-        {
-            throw new NotImplementedException();
+                        return response.Content;
+                    }, 
+                cancellationToken);
         }
 
         /// <summary>Finds the <see cref="Item"/> with the specified identifier.</summary>
@@ -167,6 +143,59 @@ namespace GW2DotNET.V2.Items
         /// <param name="cancellationToken">The <see cref="CancellationToken"/> that provides cancellation support.</param>
         /// <returns>The <see cref="Item"/> with the specified identifier.</returns>
         public Task<Item> FindAsync(int identifier, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>Gets a page with the specified page number.</summary>
+        /// <param name="page">The page to get.</param>
+        /// <returns>The page.</returns>
+        public ICollectionPage<Item> GetPage(int page)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>Gets a page with the specified page number and maximum size.</summary>
+        /// <param name="page">The page to get.</param>
+        /// <param name="pageSize">The maximum number of page elements.</param>
+        /// <returns>The page.</returns>
+        public ICollectionPage<Item> GetPage(int page, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>Gets a page with the specified page number.</summary>
+        /// <param name="page">The page to get.</param>
+        /// <returns>The page.</returns>
+        public Task<ICollectionPage<Item>> GetPageAsync(int page)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>Gets a page with the specified page number.</summary>
+        /// <param name="page">The page to get.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> that provides cancellation support.</param>
+        /// <returns>The page.</returns>
+        public Task<ICollectionPage<Item>> GetPageAsync(int page, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>Gets a page with the specified page number.</summary>
+        /// <param name="page">The page to get.</param>
+        /// <param name="pageSize">The maximum number of page elements.</param>
+        /// <returns>The page.</returns>
+        public Task<ICollectionPage<Item>> GetPageAsync(int page, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>Gets a page with the specified page number.</summary>
+        /// <param name="page">The page to get.</param>
+        /// <param name="pageSize">The maximum number of page elements.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> that provides cancellation support.</param>
+        /// <returns>The page.</returns>
+        public Task<ICollectionPage<Item>> GetPageAsync(int page, int pageSize, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
         }
