041182d886d2bcd842309c32fb1bddd1cce38e9c
Steven Liekens <steven.liekens@gmail.com>
2014-09-27 10:22:31 +0000

    Adding stubs for item details data contracts
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET;C38303

---
+++ Code/GW2.NET.Core/V2/Items.Json/DetailsDataContract.cs
@@ -0,0 +1,73 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DetailsDataContract.cs" company="GW2.NET Coding Team">
+//   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+// <summary>
+//   The details data contract.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace GW2DotNET.V2.Items.Json
+{
+    using System.Runtime.Serialization;
+
+    /// <summary>The details data contract.</summary>
+    [DataContract]
+    internal sealed partial class DetailsDataContract
+    {
+    }
+
+    /// <summary>The armor details data contract.</summary>
+    internal sealed partial class DetailsDataContract
+    {
+    }
+
+    /// <summary>The backpack details data contract.</summary>
+    internal sealed partial class DetailsDataContract
+    {
+    }
+
+    /// <summary>The bag details data contract.</summary>
+    internal sealed partial class DetailsDataContract
+    {
+    }
+
+    /// <summary>The consumable details data contract.</summary>
+    internal sealed partial class DetailsDataContract
+    {
+    }
+
+    /// <summary>The container details data contract.</summary>
+    internal sealed partial class DetailsDataContract
+    {
+    }
+
+    /// <summary>The gathering tool details data contract.</summary>
+    internal sealed partial class DetailsDataContract
+    {
+    }
+
+    /// <summary>The gizmo details data contract.</summary>
+    internal sealed partial class DetailsDataContract
+    {
+    }
+
+    /// <summary>The tool details data contract.</summary>
+    internal sealed partial class DetailsDataContract
+    {
+    }
+
+    /// <summary>The trinket details data contract.</summary>
+    internal sealed partial class DetailsDataContract
+    {
+    }
+
+    /// <summary>The upgrade component details data contract.</summary>
+    internal sealed partial class DetailsDataContract
+    {
+    }
+
+    /// <summary>The weapon details data contract.</summary>
+    internal sealed partial class DetailsDataContract
+    {
+    }
+}
