a30883e950ca02917c365d73e1966e69829d4976
Steven Liekens <steven.liekens@gmail.com>
2014-10-08 17:46:46 +0000

    Removing navigation property 'Map' from 'AvatarContext'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET;C38489

--- Code/GW2.NET.MumbleLink/AvatarContext.cs
+++ Code/GW2.NET.MumbleLink/AvatarContext.cs
@@ -4,15 +4,11 @@ namespace GW2DotNET.MumbleLink
 {
     using System.Net;
 
-    using GW2DotNET.Entities.Maps;
-
     public class AvatarContext
     {
         public IPEndPoint ServerAddress { get; set; }
 
         public int MapId { get; set; }
-
-        public Map Map { get; set; }
 
         public int MapType { get; set; }
 
