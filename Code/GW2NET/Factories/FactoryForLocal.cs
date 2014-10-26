0622b159f68d396691d464062d319220fd8398a5
Steven Liekens <steven.liekens@gmail.com>
2014-10-26 11:43:53 +0000

    Removing the offline world name service; update your code to use /v2/worlds instead
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET;C38651

--- Code/GW2NET/Factories/FactoryForLocal.cs
+++ Code/GW2NET/Factories/FactoryForLocal.cs
@@ -1,20 +1,9 @@
 ﻿namespace GW2NET.Factories
 {
-    using GW2NET.Common.Serializers;
     using GW2NET.Local.DynamicEvents;
-    using GW2NET.Local.Worlds;
-    using GW2NET.V1.Worlds;
 
     public class FactoryForLocal
     {
-        public IWorldNameService Worlds
-        {
-            get
-            {
-                return new OfflineWorldService(new JsonSerializerFactory());
-            }
-        }
-
         public IDynamicEventRotationService EventRotations
         {
             get
--- Code/GW2NET/OfflineServiceManager.cs
+++ Code/GW2NET/OfflineServiceManager.cs
@@ -17,25 +17,18 @@ namespace GW2NET
 
     using GW2NET.Common.Serializers;
     using GW2NET.Entities.DynamicEvents;
-    using GW2NET.Entities.Worlds;
     using GW2NET.Local.DynamicEvents;
-    using GW2NET.Local.Worlds;
-    using GW2NET.V1.Worlds;
 
     /// <summary>Provides access to specialty services that do not require a connection to a remote service.</summary>
-    public class OfflineServiceManager : IDynamicEventRotationService, IWorldNameService
+    public class OfflineServiceManager : IDynamicEventRotationService
     {
         /// <summary>Infrastructure. Holds a reference to a service.</summary>
         private readonly IDynamicEventRotationService dynamicEventRotationService;
-
-        /// <summary>Infrastructure. Holds a reference to a service.</summary>
-        private readonly IWorldNameService worldNameService;
 
         /// <summary>Initializes a new instance of the <see cref="OfflineServiceManager"/> class.</summary>
         public OfflineServiceManager()
         {
             this.dynamicEventRotationService = new DynamicEventRotationService();
-            this.worldNameService = new OfflineWorldService(new JsonSerializerFactory());
         }
 
         /// <summary>Gets a collection of dynamic events and their rotating shifts</summary>
@@ -45,65 +38,11 @@ namespace GW2NET
             return this.dynamicEventRotationService.GetDynamicEventRotations();
         }
 
-        /// <summary>Gets a collection of worlds and their localized name.</summary>
-        /// <returns>A collection of worlds and their localized name.</returns>
-        /// <remarks>See <a href="http://wiki.guildwars2.com/wiki/API:1/world_names">wiki</a> for more information.</remarks>
-        public IDictionary<int, World> GetWorldNames()
-        {
-            return this.worldNameService.GetWorldNames();
-        }
-
-        /// <summary>Gets a collection of worlds and their localized name.</summary>
-        /// <param name="language">The language.</param>
-        /// <returns>A collection of worlds and their localized name.</returns>
-        /// <remarks>See <a href="http://wiki.guildwars2.com/wiki/API:1/world_names">wiki</a> for more information.</remarks>
-        public IDictionary<int, World> GetWorldNames(CultureInfo language)
-        {
-            return this.worldNameService.GetWorldNames(language);
-        }
-
-        /// <summary>Gets a collection of worlds and their localized name.</summary>
-        /// <returns>A collection of worlds and their localized name.</returns>
-        /// <remarks>See <a href="http://wiki.guildwars2.com/wiki/API:1/world_names">wiki</a> for more information.</remarks>
-        public Task<IDictionary<int, World>> GetWorldNamesAsync()
-        {
-            return this.worldNameService.GetWorldNamesAsync();
-        }
-
-        /// <summary>Gets a collection of worlds and their localized name.</summary>
-        /// <param name="cancellationToken">The <see cref="CancellationToken"/> that provides cancellation support.</param>
-        /// <returns>A collection of worlds and their localized name.</returns>
-        /// <remarks>See <a href="http://wiki.guildwars2.com/wiki/API:1/world_names">wiki</a> for more information.</remarks>
-        public Task<IDictionary<int, World>> GetWorldNamesAsync(CancellationToken cancellationToken)
-        {
-            return this.worldNameService.GetWorldNamesAsync(cancellationToken);
-        }
-
-        /// <summary>Gets a collection of worlds and their localized name.</summary>
-        /// <param name="language">The language.</param>
-        /// <returns>A collection of worlds and their localized name.</returns>
-        /// <remarks>See <a href="http://wiki.guildwars2.com/wiki/API:1/world_names">wiki</a> for more information.</remarks>
-        public Task<IDictionary<int, World>> GetWorldNamesAsync(CultureInfo language)
-        {
-            return this.worldNameService.GetWorldNamesAsync(language);
-        }
-
-        /// <summary>Gets a collection of worlds and their localized name.</summary>
-        /// <param name="language">The language.</param>
-        /// <param name="cancellationToken">The <see cref="CancellationToken"/> that provides cancellation support.</param>
-        /// <returns>A collection of worlds and their localized name.</returns>
-        /// <remarks>See <a href="http://wiki.guildwars2.com/wiki/API:1/world_names">wiki</a> for more information.</remarks>
-        public Task<IDictionary<int, World>> GetWorldNamesAsync(CultureInfo language, CancellationToken cancellationToken)
-        {
-            return this.worldNameService.GetWorldNamesAsync(language, cancellationToken);
-        }
-
         /// <summary>The invariant method for this class.</summary>
         [ContractInvariantMethod]
         private void ObjectInvariant()
         {
             Contract.Invariant(this.dynamicEventRotationService != null);
-            Contract.Invariant(this.worldNameService != null);
         }
     }
 }
