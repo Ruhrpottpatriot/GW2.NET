2187d4e47ac11731abf2c187cc99a7276333bba5
Steven Liekens <steven.liekens@gmail.com>
2014-10-28 22:00:15 +0000

    Fixing a weird copy paste error
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET;C38685

--- Code/GW2NET.Core/Common/IRepository.cs
+++ Code/GW2NET.Core/Common/IRepository.cs
@@ -22,40 +22,40 @@ namespace GW2NET.Common
     {
         /// <summary>Finds the object of type <typeparamref name="TValue"/> with the identifier of type <typeparamref name="TKey"/>.</summary>
         /// <param name="identifier">The identifier of the object to find.</param>
-        /// <exception cref="NotSupportedException">The data source does not finding individual objects.</exception>
+        /// <exception cref="NotSupportedException">The data source does not support finding individual objects.</exception>
         /// <exception cref="ServiceException">An error occurred while retrieving data from the data source.</exception>
         /// <returns>The object with the specified identifier, or a null reference.</returns>
         TValue Find(TKey identifier);
 
         /// <summary>Finds every object of type <typeparamref name="TValue"/>.</summary>
-        /// <exception cref="NotSupportedException">The data source does not finding all objects.</exception>
+        /// <exception cref="NotSupportedException">The data source does not support finding all objects.</exception>
         /// <exception cref="ServiceException">An error occurred while retrieving data from the data source.</exception>
         /// <returns>A collection of objects.</returns>
         IDictionaryRange<TKey, TValue> FindAll();
 
         /// <summary>Finds every object with one of the specified identifiers.</summary>
         /// <param name="identifiers">The identifiers of the objects to find.</param>
-        /// <exception cref="NotSupportedException">The data source does not finding a range of objects.</exception>
+        /// <exception cref="NotSupportedException">The data source does not support finding a range of objects.</exception>
         /// <exception cref="ServiceException">An error occurred while retrieving data from the data source.</exception>
         /// <returns>A collection of objects with one of the specified identifiers.</returns>
         IDictionaryRange<TKey, TValue> FindAll(ICollection<TKey> identifiers);
 
         /// <summary>Finds every object of type <typeparamref name="TValue"/>.</summary>
-        /// <exception cref="NotSupportedException">The data source does not finding all objects.</exception>
+        /// <exception cref="NotSupportedException">The data source does not support finding all objects.</exception>
         /// <exception cref="ServiceException">An error occurred while retrieving data from the data source.</exception>
         /// <returns>A collection of objects.</returns>
         Task<IDictionaryRange<TKey, TValue>> FindAllAsync();
 
         /// <summary>Finds every object of type <typeparamref name="TValue"/>.</summary>
         /// <param name="cancellationToken">The <see cref="CancellationToken"/> that provides cancellation support.</param>
-        /// <exception cref="NotSupportedException">The data source does not finding all objects.</exception>
+        /// <exception cref="NotSupportedException">The data source does not support finding all objects.</exception>
         /// <exception cref="ServiceException">An error occurred while retrieving data from the data source.</exception>
         /// <returns>A collection of objects.</returns>
         Task<IDictionaryRange<TKey, TValue>> FindAllAsync(CancellationToken cancellationToken);
 
         /// <summary>Finds every object with one of the specified identifiers.</summary>
         /// <param name="identifiers">The identifiers of the objects to find.</param>
-        /// <exception cref="NotSupportedException">The data source does not finding a range of objects.</exception>
+        /// <exception cref="NotSupportedException">The data source does not support finding a range of objects.</exception>
         /// <exception cref="ServiceException">An error occurred while retrieving data from the data source.</exception>
         /// <returns>A collection of objects with one of the specified identifiers.</returns>
         Task<IDictionaryRange<TKey, TValue>> FindAllAsync(ICollection<TKey> identifiers);
@@ -63,14 +63,14 @@ namespace GW2NET.Common
         /// <summary>Finds every object with one of the specified identifiers.</summary>
         /// <param name="identifiers">The identifiers of the objects to find.</param>
         /// <param name="cancellationToken">The <see cref="CancellationToken"/> that provides cancellation support.</param>
-        /// <exception cref="NotSupportedException">The data source does not finding a range of objects.</exception>
+        /// <exception cref="NotSupportedException">The data source does not support finding a range of objects.</exception>
         /// <exception cref="ServiceException">An error occurred while retrieving data from the data source.</exception>
         /// <returns>A collection of objects with one of the specified identifiers.</returns>
         Task<IDictionaryRange<TKey, TValue>> FindAllAsync(ICollection<TKey> identifiers, CancellationToken cancellationToken);
 
         /// <summary>Finds the object of type <typeparamref name="TValue"/> with the identifier of type <typeparamref name="TKey"/>.</summary>
         /// <param name="identifier">The identifier of the object to find.</param>
-        /// <exception cref="NotSupportedException">The data source does not finding individual objects.</exception>
+        /// <exception cref="NotSupportedException">The data source does not support finding individual objects.</exception>
         /// <exception cref="ServiceException">An error occurred while retrieving data from the data source.</exception>
         /// <returns>The object with the specified identifier, or a null reference.</returns>
         Task<TValue> FindAsync(TKey identifier);
@@ -78,7 +78,7 @@ namespace GW2NET.Common
         /// <summary>Finds the object of type <typeparamref name="TValue"/> with the identifier of type <typeparamref name="TKey"/>.</summary>
         /// <param name="identifier">The identifier of the object to find.</param>
         /// <param name="cancellationToken">The <see cref="CancellationToken"/> that provides cancellation support.</param>
-        /// <exception cref="NotSupportedException">The data source does not finding individual objects.</exception>
+        /// <exception cref="NotSupportedException">The data source does not support finding individual objects.</exception>
         /// <exception cref="ServiceException">An error occurred while retrieving data from the data source.</exception>
         /// <returns>The object with the specified identifier, or a null reference.</returns>
         Task<TValue> FindAsync(TKey identifier, CancellationToken cancellationToken);
