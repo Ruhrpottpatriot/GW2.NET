892e12118fac36405d4da628c96ca6cb5b246afe
Steven Liekens <steven.liekens@gmail.com>
2014-11-04 19:58:39 +0000

    Refactoring 'ConverterForGatheringTools'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET;C38752

--- Code/GW2NET.Core/V2/Items/Converters/ConverterForGatheringTools.cs
+++ Code/GW2NET.Core/V2/Items/Converters/ConverterForGatheringTools.cs
@@ -8,6 +8,8 @@
 // --------------------------------------------------------------------------------------------------------------------
 namespace GW2NET.V2.Items.Converters
 {
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
     using System.Diagnostics.Contracts;
 
     using GW2NET.Common;
@@ -17,23 +19,55 @@ namespace GW2NET.V2.Items.Converters
     /// <summary>Converts objects of type <see cref="DetailsDataContract"/> to objects of type <see cref="GatheringTool"/>.</summary>
     internal sealed class ConverterForGatheringTool : IConverter<DetailsDataContract, GatheringTool>
     {
+        /// <summary>Infrastructure. Holds a reference to a collection of type converters.</summary>
+        private readonly IDictionary<string, IConverter<DetailsDataContract, GatheringTool>> typeConverters;
+
+        /// <summary>Initializes a new instance of the <see cref="ConverterForGatheringTool"/> class.</summary>
+        internal ConverterForGatheringTool()
+            : this(GetKnownTypeConverters())
+        {
+        }
+
+        /// <summary>Initializes a new instance of the <see cref="ConverterForGatheringTool"/> class.</summary>
+        /// <param name="typeConverters">The type converters.</param>
+        internal ConverterForGatheringTool(IDictionary<string, IConverter<DetailsDataContract, GatheringTool>> typeConverters)
+        {
+            Contract.Requires(typeConverters != null);
+            this.typeConverters = typeConverters;
+        }
+
         /// <summary>Converts the given object of type <see cref="DetailsDataContract"/> to an object of type <see cref="GatheringTool"/>.</summary>
         /// <param name="value">The value to convert.</param>
         /// <returns>The converted value.</returns>
         public GatheringTool Convert(DetailsDataContract value)
         {
             Contract.Assume(value != null);
-            switch (value.Type)
+            IConverter<DetailsDataContract, GatheringTool> converter;
+            if (this.typeConverters.TryGetValue(value.Type, out converter))
             {
-                case "Foraging":
-                    return new ForagingTool();
-                case "Logging":
-                    return new LoggingTool();
-                case "Mining":
-                    return new MiningTool();
-                default:
-                    return new UnknownGatheringTool();
+                return converter.Convert(value);
             }
+
+            return new UnknownGatheringTool();
+        }
+
+        /// <summary>Infrastructure. Gets default type converters for all known types.</summary>
+        /// <returns>The type converters.</returns>
+        private static IDictionary<string, IConverter<DetailsDataContract, GatheringTool>> GetKnownTypeConverters()
+        {
+            return new Dictionary<string, IConverter<DetailsDataContract, GatheringTool>>
+            {
+                { "Foraging", new ConverterForForagingTool() }, 
+                { "Logging", new ConverterForLoggingTool() }, 
+                { "Mining", new ConverterForMiningTool() }, 
+            };
+        }
+
+        [ContractInvariantMethod]
+        [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "Only used by the Code Contracts for .NET extension.")]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this.typeConverters != null);
         }
     }
 }
