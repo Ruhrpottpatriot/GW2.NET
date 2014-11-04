4f3255a8865670c4d35f9aeadca65b3dcfa48263
Steven Liekens <steven.liekens@gmail.com>
2014-11-04 20:07:38 +0000

    Refactoring 'ConverterForGizmo'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/GW2.NET;C38754

--- Code/GW2NET.Core/V2/Items/Converters/ConverterForGizmo.cs
+++ Code/GW2NET.Core/V2/Items/Converters/ConverterForGizmo.cs
@@ -8,6 +8,8 @@
 // --------------------------------------------------------------------------------------------------------------------
 namespace GW2NET.V2.Items.Converters
 {
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
     using System.Diagnostics.Contracts;
 
     using GW2NET.Common;
@@ -17,25 +19,56 @@ namespace GW2NET.V2.Items.Converters
     /// <summary>Converts objects of type <see cref="DetailsDataContract"/> to objects of type <see cref="Gizmo"/>.</summary>
     internal sealed class ConverterForGizmo : IConverter<DetailsDataContract, Gizmo>
     {
+        /// <summary>Infrastructure. Holds a reference to a collection of type converters.</summary>
+        private readonly IDictionary<string, IConverter<DetailsDataContract, Gizmo>> typeConverters;
+
+        /// <summary>Initializes a new instance of the <see cref="ConverterForGizmo"/> class.</summary>
+        internal ConverterForGizmo()
+            : this(GetKnownTypeConverters())
+        {
+        }
+
+        /// <summary>Initializes a new instance of the <see cref="ConverterForGizmo"/> class.</summary>
+        /// <param name="typeConverters">The type converters.</param>
+        internal ConverterForGizmo(IDictionary<string, IConverter<DetailsDataContract, Gizmo>> typeConverters)
+        {
+            Contract.Requires(typeConverters != null);
+            this.typeConverters = typeConverters;
+        }
+
         /// <summary>Converts the given object of type <see cref="DetailsDataContract"/> to an object of type <see cref="Gizmo"/>.</summary>
         /// <param name="value">The value to convert.</param>
         /// <returns>The converted value.</returns>
         public Gizmo Convert(DetailsDataContract value)
         {
             Contract.Assume(value != null);
-            switch (value.Type)
+            IConverter<DetailsDataContract, Gizmo> converter;
+            if (this.typeConverters.TryGetValue(value.Type, out converter))
             {
-                case "Default":
-                    return new DefaultGizmo();
-                case "ContainerKey":
-                    return new ContainerKey();
-                case "RentableContractNpc":
-                    return new RentableContractNpc();
-                case "UnlimitedConsumable":
-                    return new UnlimitedConsumable();
-                default:
-                    return new UnknownGizmo();
+                return converter.Convert(value);
             }
+
+            return new UnknownGizmo();
+        }
+
+        /// <summary>Infrastructure. Gets default type converters for all known types.</summary>
+        /// <returns>The type converters.</returns>
+        private static IDictionary<string, IConverter<DetailsDataContract, Gizmo>> GetKnownTypeConverters()
+        {
+            return new Dictionary<string, IConverter<DetailsDataContract, Gizmo>>
+            {
+                { "Default", new ConverterForDefaultGizmo() }, 
+                { "ContainerKey", new ConverterForContainerKey() }, 
+                { "RentableContractNpc", new ConverterForRentableContractNpc() }, 
+                { "UnlimitedConsumable", new ConverterForUnlimitedConsumable() }, 
+            };
+        }
+
+        [ContractInvariantMethod]
+        [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "Only used by the Code Contracts for .NET extension.")]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(this.typeConverters != null);
         }
     }
 }
