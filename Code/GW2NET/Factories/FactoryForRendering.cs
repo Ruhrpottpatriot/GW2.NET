1de54bf5452a6674dd0d777caa548a0fd671ff4b
Steven Liekens <steven.liekens@gmail.com>
2014-11-26 13:58:08 +0000

    Adding missing files
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/Master;C38987

---
+++ Code/GW2NET/Factories/FactoryForRendering.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="FactoryForRendering.cs" company="GW2.NET Coding Team">
+//   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+// <summary>
+//   Provides access to the rendering service.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace GW2NET.Factories
+{
+    using System.Diagnostics.Contracts;
+
+    using GW2NET.Common;
+    using GW2NET.Rendering;
+
+    /// <summary>Provides access to the rendering service.</summary>
+    public class FactoryForRendering : FactoryBase
+    {
+        /// <summary>Initializes a new instance of the <see cref="FactoryForRendering"/> class.</summary>
+        /// <param name="serviceClient">The service client.</param>
+        public FactoryForRendering(IServiceClient serviceClient)
+            : base(serviceClient)
+        {
+            Contract.Requires(serviceClient != null);
+        }
+
+        /// <summary>Provides access to the rendering service.</summary>
+        public IRenderService RenderService
+        {
+            get
+            {
+                Contract.Ensures(Contract.Result<IRenderService>() != null);
+                return new RenderService(this.ServiceClient);
+            }
+        }
+    }
+}
