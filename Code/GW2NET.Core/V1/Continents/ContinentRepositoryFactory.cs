6504297849c08f6c59da5bb27061588ab4f776d0
Steven Liekens <steven.liekens@gmail.com>
2014-12-29 15:57:13 +0000

    ContinentRepositoryFactory: removing unused parameter 'continentId'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/Release;C39227

--- Code/GW2NET.Core/V1/Continents/ContinentRepositoryFactory.cs
+++ Code/GW2NET.Core/V1/Continents/ContinentRepositoryFactory.cs
@@ -84,9 +84,8 @@ namespace GW2NET.V1.Continents
         }
 
         /// <summary>Creates an instance for the current UI language.</summary>
-        /// <param name="continentId">The continent identifier.</param>
         /// <returns>A repository.</returns>
-        public IContinentRepository ForCurrentUICulture(int continentId)
+        public IContinentRepository ForCurrentUICulture()
         {
             Contract.Ensures(Contract.Result<IContinentRepository>() != null);
             return this.ForCulture(CultureInfo.CurrentUICulture);
