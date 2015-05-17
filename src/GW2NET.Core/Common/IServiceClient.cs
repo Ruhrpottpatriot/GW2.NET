b6a697d93fe8ffba86259b611aba56341f58356d
Steven Liekens <steven.liekens@gmail.com>
2015-05-17 15:42:57 +0000

    Adding argument exception docs to 'IServiceClient'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/Master;C40743

--- src/GW2NET.Core/Common/IServiceClient.cs
+++ src/GW2NET.Core/Common/IServiceClient.cs
@@ -8,6 +8,7 @@
 // --------------------------------------------------------------------------------------------------------------------
 namespace GW2NET.Common
 {
+    using System;
     using System.Diagnostics.Contracts;
     using System.Threading;
     using System.Threading.Tasks;
@@ -19,12 +20,14 @@ namespace GW2NET.Common
         /// <summary>Sends a request and returns the response.</summary>
         /// <param name="request">The service request.</param>
         /// <typeparam name="TResult">The type of the response content.</typeparam>
+        /// <exception cref="ArgumentNullException">The value of <paramref name="request"/> is a null reference.</exception>
         /// <returns>An instance of the specified type.</returns>
         IResponse<TResult> Send<TResult>(IRequest request);
 
         /// <summary>Sends a request and returns the response.</summary>
         /// <param name="request">The service request.</param>
         /// <typeparam name="TResult">The type of the response content.</typeparam>
+        /// <exception cref="ArgumentNullException">The value of <paramref name="request"/> is a null reference.</exception>
         /// <returns>An instance of the specified type.</returns>
         Task<IResponse<TResult>> SendAsync<TResult>(IRequest request);
 
@@ -32,6 +35,7 @@ namespace GW2NET.Common
         /// <param name="request">The service request.</param>
         /// <param name="cancellationToken">The <see cref="CancellationToken"/> that provides cancellation support.</param>
         /// <typeparam name="TResult">The type of the response content.</typeparam>
+        /// <exception cref="ArgumentNullException">The value of <paramref name="request"/> is a null reference.</exception>
         /// <returns>An instance of the specified type.</returns>
         Task<IResponse<TResult>> SendAsync<TResult>(IRequest request, CancellationToken cancellationToken);
     }
