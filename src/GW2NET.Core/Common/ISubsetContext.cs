03ab9250c445fd8924a4c93258d00ed20cf53b75
Steven Liekens <steven.liekens@gmail.com>
2015-05-17 15:18:20 +0000

    Adding argument exception docs to 'ISubsetContext'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/Master;C40736

--- src/GW2NET.Core/Common/ISubsetContext.cs
+++ src/GW2NET.Core/Common/ISubsetContext.cs
@@ -8,6 +8,7 @@
 // --------------------------------------------------------------------------------------------------------------------
 namespace GW2NET.Common
 {
+    using System;
     using System.Diagnostics.Contracts;
 
     /// <summary>Provides contextual information for collections that are a subset of a larger collection.</summary>
@@ -15,9 +16,11 @@ namespace GW2NET.Common
     public interface ISubsetContext
     {
         /// <summary>Gets or sets the number of values in this subset.</summary>
+        /// <exception cref="ArgumentOutOfRangeException">The given value is negative.</exception>
         int SubtotalCount { get; set; }
 
         /// <summary>Gets or sets the number of values in the collection.</summary>
+        /// <exception cref="ArgumentOutOfRangeException">The given value is negative.</exception>
         int TotalCount { get; set; }
     }
 }
