b416b087cdccf2203743ec9bf01338efe9a1db1b
Steven Liekens <steven.liekens@gmail.com>
2015-05-17 15:35:31 +0000

    Adding argument exception docs to 'IRenderService'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/Master;C40739

--- src/GW2NET.Core/Common/IRenderService.cs
+++ src/GW2NET.Core/Common/IRenderService.cs
@@ -8,6 +8,7 @@
 // --------------------------------------------------------------------------------------------------------------------
 namespace GW2NET.Common
 {
+    using System;
     using System.Diagnostics.Contracts;
     using System.Threading;
     using System.Threading.Tasks;
@@ -20,6 +21,8 @@ namespace GW2NET.Common
         /// <summary>Gets binary image data for the given file identifier and image format.</summary>
         /// <param name="file">The file identifier.</param>
         /// <param name="imageFormat">The image file format.</param>
+        /// <exception cref="ArgumentNullException">The value of <paramref name="file"/> or <paramref name="imageFormat"/> is a null reference.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">The value of <paramref name="imageFormat"/> is not "jpg" or "png".</exception>
         /// <exception cref="ServiceException">An error occurred while retrieving the image data.</exception>
         /// <returns>The binary image data.</returns>
         byte[] GetImage(IRenderable file, string imageFormat);
@@ -27,6 +30,8 @@ namespace GW2NET.Common
         /// <summary>Gets binary image data for the given file identifier and image format.</summary>
         /// <param name="file">The file identifier.</param>
         /// <param name="imageFormat">The image format.</param>
+        /// <exception cref="ArgumentNullException">The value of <paramref name="file"/> or <paramref name="imageFormat"/> is a null reference.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">The value of <paramref name="imageFormat"/> is not "jpg" or "png".</exception>
         /// <exception cref="ServiceException">An error occurred while retrieving the image data.</exception>
         /// <returns>The binary image data.</returns>
         Task<byte[]> GetImageAsync(IRenderable file, string imageFormat);
@@ -35,6 +40,8 @@ namespace GW2NET.Common
         /// <param name="file">The file identifier.</param>
         /// <param name="imageFormat">The image format.</param>
         /// <param name="cancellationToken">The <see cref="CancellationToken"/> that provides cancellation support.</param>
+        /// <exception cref="ArgumentNullException">The value of <paramref name="file"/> or <paramref name="imageFormat"/> is a null reference.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">The value of <paramref name="imageFormat"/> is not "jpg" or "png".</exception>
         /// <exception cref="ServiceException">An error occurred while retrieving the image data.</exception>
         /// <exception cref="TaskCanceledException">A task was canceled.</exception>
         /// <returns>The binary image data.</returns>
