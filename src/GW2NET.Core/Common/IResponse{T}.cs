d9650050979eeb7d62f5eb60066902d4dd9e52b7
Steven Liekens <steven.liekens@gmail.com>
2015-05-25 21:01:56 +0000

    Extracting non-generic interface 'IResponse' from 'IResponse<T>'
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/Master;C40837

---
+++ src/GW2NET.Core/Common/IResponse{T}.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IResponse.cs" company="GW2.NET Coding Team">
+//   This product is licensed under the GNU General Public License version 2 (GPLv2) as defined on the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+// <summary>
+//   Provides the interface for service responses.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace GW2NET.Common
+{
+    /// <summary>Provides the interface for service responses.</summary>
+    /// <typeparam name="T">The type of the response content.</typeparam>
+    public interface IResponse<T> : IResponse
+    {
+        /// <summary>Gets or sets the response content.</summary>
+        T Content { get; set; }
+    }
+}
