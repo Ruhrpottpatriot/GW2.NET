701421addf38c3dd77af2b37084881697b38aafb
Steven Liekens <steven.liekens@gmail.com>
2015-05-26 21:34:20 +0000

    Adding ColorPaletteConverterTests
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/Master;C40870

---
+++ src/GW2NET.V2.Colors.Tests/Converters/ColorModelConverterMock.cs
@@ -0,0 +1,16 @@
+﻿namespace GW2NET.V2.Colors.Converters
+{
+    using GW2NET.Colors;
+    using GW2NET.Common;
+
+    public class ColorModelConverterMock : IConverter<ColorModelDataContract, ColorModel>
+    {
+        public int ConvertCount { get; set; }
+
+        public ColorModel Convert(ColorModelDataContract value, object state)
+        {
+            this.ConvertCount += 1;
+            return default(ColorModel);
+        }
+    }
+}
---
+++ src/GW2NET.V2.Colors.Tests/Converters/ColorPaletteConverterTests.cs
@@ -0,0 +1,55 @@
+﻿namespace GW2NET.V2.Colors.Converters
+{
+    using System.Globalization;
+
+    using GW2NET.Colors;
+    using GW2NET.Common;
+
+    using Xunit;
+
+    public class ColorPaletteConverterTests
+    {
+        private readonly ColorConverterMock colorConverterMock;
+        private readonly ColorModelConverterMock colorModelConverterMock;
+        private readonly ColorPaletteConverter colorPaletteConverter;
+
+        public ColorPaletteConverterTests()
+        {
+            this.colorConverterMock = new ColorConverterMock();
+            this.colorModelConverterMock = new ColorModelConverterMock();
+            this.colorPaletteConverter = new ColorPaletteConverter(this.colorConverterMock, this.colorModelConverterMock);
+        }
+
+        [Theory]
+        [InlineData("de", 1, "Farbentferner", new[] { 128, 26, 26 })]
+        [InlineData("en", 1, "Dye Remover", new[] { 128, 26, 26 })]
+        [InlineData("es", 1, "Disolvente de tinte", new[] { 128, 26, 26 })]
+        [InlineData("fr", 1, "Dissolvant pour teinture", new[] { 128, 26, 26 })]
+        public void CanConvert(string lang, int id, string name, int[] baseRgb)
+        {
+            var value = new ColorPaletteDataContract
+            {
+                Id = id,
+                Name = name,
+                BaseRgb = baseRgb,
+                Cloth = new ColorModelDataContract(),
+                Leather = new ColorModelDataContract(),
+                Metal = new ColorModelDataContract()
+            };
+
+            var state = new Response<ColorPaletteDataContract>
+            {
+                Content = value,
+                Culture = new CultureInfo(lang)
+            };
+
+            var result = this.colorPaletteConverter.Convert(value, state);
+            Assert.NotNull(result);
+            Assert.Equal(state.Culture, result.Culture);
+            Assert.Equal(id, result.ColorId);
+            Assert.Equal(name, result.Name);
+            Assert.Equal(1, this.colorConverterMock.ConvertCount);
+            Assert.Equal(3, this.colorModelConverterMock.ConvertCount);
+        }
+    }
+}
