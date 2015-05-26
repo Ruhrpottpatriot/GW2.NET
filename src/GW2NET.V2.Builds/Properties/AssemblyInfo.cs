9db2b40c39942fa31ff4a06c29517c3ea9ab8ceb
Steven Liekens <steven.liekens@gmail.com>
2015-05-26 18:13:19 +0000

    Adding unit test project templates for every V1 and V2 project
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/Master;C40852

--- src/GW2NET.V2.Builds/Properties/AssemblyInfo.cs
+++ src/GW2NET.V2.Builds/Properties/AssemblyInfo.cs
@@ -10,3 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("This library implements /v2/build")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
+
+[assembly: InternalsVisibleTo("GW2NET.V2.Builds.Tests")]
