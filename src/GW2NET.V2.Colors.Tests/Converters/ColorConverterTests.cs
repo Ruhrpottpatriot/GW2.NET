a545c68fdeef4b07b4488bc242fb326b39641ed0
Steven Liekens <steven.liekens@gmail.com>
2015-05-26 19:01:07 +0000

    Adding ColorConverterTests
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/gw2dotnet/Master;C40862

---
+++ src/GW2NET.V2.Colors.Tests/Converters/ColorConverterTests.cs
@@ -0,0 +1,24 @@
+﻿namespace GW2NET.V2.Colors.Converters
+{
+    using Xunit;
+
+    public class ColorConverterTests
+    {
+        private readonly ColorConverter converter = new ColorConverter();
+
+        [Theory]
+        [InlineData(128, 26, 26)]
+        [InlineData(124, 108, 83)]
+        [InlineData(65, 49, 29)]
+        [InlineData(96, 91, 83)]
+        public void CanConvert(int red, int green, int blue)
+        {
+            var value = new[] { red, green, blue };
+            var result = this.converter.Convert(value, null);
+            Assert.NotNull(result);
+            Assert.Equal(red, result.R);
+            Assert.Equal(green, result.G);
+            Assert.Equal(blue, result.B);
+        }
+    }
+}
