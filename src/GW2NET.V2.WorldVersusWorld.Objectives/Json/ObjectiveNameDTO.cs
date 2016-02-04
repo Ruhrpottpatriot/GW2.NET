9d331c11cd120edab507c09171facf41566b769d
ZedTheYeti <zedtheyeti@gmail.com>
2016-02-03 21:24:30 -0600

    Added barebones structure for V2 WvW. Following the V1 structure. No code just classes.

---
+++ src/GW2NET.V2.WorldVersusWorld.Objectives/Json/ObjectiveNameDTO.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ObjectiveNameDTO.cs" company="GW2.NET Coding Team">
+//   This product is licensed under the GNU General Public License version 2 (GPLv2). See the License in the project root folder or the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+// <summary>
+//   Defines the ObjectiveNameDTO type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GW2NET.V2.WorldVersusWorld.Objectives.Json
+{
+    class ObjectiveNameDTO
+    {
+    }
+}
