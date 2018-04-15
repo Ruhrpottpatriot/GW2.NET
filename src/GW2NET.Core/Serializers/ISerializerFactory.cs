de8ec5d55dcb577832d873fbfd84ec5811b2a799
Robert Logiewa <Ruhrpottpatriot@gmx.de>
2018-04-15 18:37:39 +0200

    Add serializer interfaces

---
+++ src/GW2NET.Core/Serializers/ISerializerFactory.cs
@@ -0,0 +1,19 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ISerializerFactory.cs" company="GW2.NET Coding Team">
+//   This product is licensed under the GNU General Public License version 2 (GPLv2). See the License in the project root folder or the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+// <summary>
+//   Provides the interface for serialization engine factories.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace GW2NET.Common.Serializers
+{
+    /// <summary>Provides the interface for serialization engine factories.</summary>
+    public interface ISerializerFactory
+    {
+        /// <summary>Gets a serializer for the specified type.</summary>
+        /// <typeparam name="T">The serialization type.</typeparam>
+        /// <returns>The <see cref="ISerializer{T}"/>.</returns>
+        ISerializer<T> GetSerializer<T>();
+    }
+}
